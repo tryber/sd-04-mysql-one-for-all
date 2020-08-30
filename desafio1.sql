9e0a14c84a3af97ab9f69032f63a7947752698fe
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-30 16:58:58 -0300

    test req01

---
+++ desafio1.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+CREATE DATABASE SpotifyClone;
+USE SpotifyClone;
+CREATE TABLE plano(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  valor DECIMAL(6, 2)
+) engine = InnoDB;
+CREATE TABLE usuario(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  idade INT NULL,
+  plano_id INT NOT NULL,
+  FOREIGN KEY(plano_id) REFERENCES plano(id)
+) engine = InnoDB;
+CREATE TABLE artista(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL
+) engine = InnoDB;
+CREATE TABLE album (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  artista_id INT NOT NULL,
+  FOREIGN KEY (artista_id) REFERENCES artista(id)
+) engine = InnoDB;
+CREATE TABLE cancao(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  album_id INT NOT NULL,
+  FOREIGN KEY(album_id) REFERENCES album(id)
+) engine = InnoDB;
+CREATE TABLE Playlist_track(
+  artista_id INT NOT NULL,
+  FOREIGN KEY (artista_id) REFERENCES artista(id),
+  usuario_id INT NOT NULL,
+  FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+CREATE TABLE Playlist_history(
+  artista_id INT NOT NULL,
+  FOREIGN KEY (artista_id) REFERENCES artista(id),
+  cancao_id INT NOT NULL,
+  FOREIGN KEY (cancao_id) REFERENCES cancao(id)
+) engine = InnoDB;
