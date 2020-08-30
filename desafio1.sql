a97d4580a739b5580b5bbd19c22fde9ddad622eb
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-30 18:52:36 -0300

    test req04

--- desafio1.sql
+++ desafio1.sql
@@ -47,3 +47,66 @@ VALUES
   ('gratuito', 0),
   ('familiar', 7.99),
   ('universitário', 5.99);
+INSERT INTO
+  usuario (nome, idade, plano_id)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+INSERT INTO
+  artista (nome,)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+INSERT INTO
+  album (nome, artista_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Exuberant', 1),
+  ('Temporary Culture', 4);
+INSERT INTO
+  cancao (
+    nome,
+    album_id
+    VALUES
+      ('Soul For Us"', 1),
+      ('Reflections Of Magic', 1),
+      ('Dance With Her Own', 1),
+      ('Troubles Of My Inner Fire"', 2),
+      ('Time Fireworks', 2),
+      ('Magic Circus', 3),
+      ('ExubeHoney, So Do Irant', 3),
+      (`Sweetie, Let's Go Wild`, 3),
+      ('She Knows', 3),
+      ('Fantasy For Me', 4),
+      ('Celebration Of More', 4),
+      ('Rock His Everything', 4),
+      ('Home Forever', 4),
+      ('Diamond Power', 4),
+      (`Honey, Let's Be Silly`, 4),
+      ('Thang Of Thunder', 5),
+      ('Words Of Her Life', 5),
+      ('Without My Streets', 5);
+    INSERT INTO
+      artiste_followed (artista_id, usuario_id)
+    VALUES
+      (1, 1),
+      (1, 4),
+      (1, 3),
+      (2, 1),
+      (2, 3),
+      (3, 2),
+      (3, 1),
+      (4, 4);
+    INSERT INTO
+      playlist_history (usuario_id, cancao_id)
+    VALUES
+      (1, 1),
+      (1, 6),
+      (1, 14),
+      (1, 16),
