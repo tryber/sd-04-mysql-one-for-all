957e4b943259fbd5adac8327f11069d02cbdf119
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-31 17:02:46 -0300

    cc v1

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,4 @@
 USE SpotifyClone;
-
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(M.nome) AS `cancoes`,
 COUNT(A.nome_artista) AS `artistas`,
@@ -7,5 +6,4 @@ COUNT(AB.nome_do_album) AS `albuns`
 FROM SpotifyClone.musica AS M
 INNER JOIN SpotifyClone.artista AS A
 INNER JOIN SpotifyClone.album As AB;
-
 SELECT * FROM estatisticas_musicais;
