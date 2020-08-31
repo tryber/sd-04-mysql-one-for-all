fefa7934ab8e8bd60c43e1b1b8f6348a210d582d
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-31 20:45:58 -0300

    v2

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
 AFTER DELETE ON usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.artista_seguindo WHERE usuario_id = OLD.id;
-DELETE FROM SpotifyClone.historico_reproducoes WHERE usuario_id = OLD.id;
+DELETE FROM SpotifyClone.artista_seguindo WHERE usuario_id = OLD.usuario.id;
+DELETE FROM SpotifyClone.historico_reproducoes WHERE usuario_id = OLD.usuario.id;
 END; $$
 DELIMITER ;
