59f92091881a751f10fc8181705f8d4c7f1e7498
AliceAAtalla <aliceatalla36@gmail.com>
2020-08-31 19:16:24 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+
+CREATE VIEW perfil_artistas AS
+SELECT
+  ar.nome AS 'artista',
+  al.titulo AS 'album',
+  count(s.artistas_id) AS 'seguidores'
+FROM SpotifyClone.artistas AS ar
+INNER JOIN SpotifyClone.albuns AS al ON ar.id = al.artista_id
+INNER JOIN SpotifyClone.artistas_seguidos AS s ON ar.id = s.artistas_id
+GROUP BY ar.nome, al.titulo
+ORDER BY
+  `seguidores` DESC,
+  `artista`,
+  `album`;
