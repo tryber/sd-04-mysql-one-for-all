bb46c65f3881f68feb131d8234e4de3758cca684
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-31 18:14:19 -0300

    ate desafio 4 funcionando no workbranch

--- desafio4.sql
+++ desafio4.sql
@@ -6,5 +6,4 @@ COUNT(ArtSe.artista_id) AS `seguidores`
 FROM SpotifyClone.artista AS A
 INNER JOIN SpotifyClone.artista_seguindo AS ArtSe WHERE A.id = ArtSe.artista_id
 GROUP BY ArtSe.artista_id
-ORDER BY `seguidores` DESC LIMIT 3;
-SELECT * FROM top_3_artistas;
+ORDER BY `seguidores` DESC, `artista` LIMIT 3;
