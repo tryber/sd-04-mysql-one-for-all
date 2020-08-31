080fc8022ad6882aaf88e74135cd5ae03b9f5fba
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-31 18:01:18 -0300

    mudand a ordem

--- desafio2.sql
+++ desafio2.sql
@@ -2,6 +2,4 @@ CREATE VIEW estatisticas_musicais AS
 SELECT 
 (SELECT COUNT(*) FROM SpotifyClone.musica) AS `cancoes`,
 (SELECT COUNT(*) FROM SpotifyClone.artista) AS `artistas`,
-(SELECT COUNT(*) FROM SpotifyClone.album) AS `albuns`;
-
-SELECT * FROM estatisticas_musicais;
+SELECT COUNT(*) FROM SpotifyClone.album AS `albuns`;
--- desafio3.sql
+++ desafio3.sql
@@ -3,7 +3,7 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT
 U.nome AS `usuario`,
 M.nome AS `nome`
-FROM SpotifyClone.usuario AS U
-INNER JOIN SpotifyClone.historico_reproducoes AS HR ON U.id = HR.usuario_id
+FROM SpotifyClone.historico_reproducoes AS HR
+INNER JOIN SpotifyClone.usuario AS U ON HR.usuario_id = U.id
 INNER JOIN SpotifyClone.musica AS M ON HR.musica_id = M.id
 ORDER BY `usuario`, `nome`;
