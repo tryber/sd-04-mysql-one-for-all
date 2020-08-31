fb285392ee342310f6ad95f0017a156dc3f02db6
Diego Campos <diegodiko05@gmail.com>
2020-08-31 06:55:04 -0300

    Desafio 6, alterando de char, o q eu discordo vieementemente

--- desafio6.sql
+++ desafio6.sql
@@ -1,30 +1,20 @@
 CREATE VIEW faturamento_atual AS
 SELECT
+  CONVERT(ROUND(MIN(plan_values), 2), char) AS `faturamento_minimo`,
+  CONVERT(ROUND(MAX(plan_values), 2), CHAR) AS `faturamento_maximo`,
+  CONVERT(ROUND(AVG(plan_values), 2), CHAR) AS `faturamento_medio`,
+  CONVERT(ROUND(SUM(plan_values), 2), CHAR) AS `faturamento_total`
+FROM
   (
     SELECT
-      round(MAX(p.valor), 2)
+      (
+        SELECT
+          valor
+        FROM
+          SpotifyClone.planos
+        WHERE
+          planos.plano_id = usuarios.plano_id
+      ) AS plan_values
     FROM
-      SpotifyClone.usuarios AS u
-      INNER JOIN SpotifyClone.planos AS p ON p.plano_id = u.plano_id
-  ) AS 'faturamento_maximo',
-  (
-    SELECT
-      round(AVG(p.valor), 2)
-    FROM
-      SpotifyClone.usuarios AS u
-      INNER JOIN SpotifyClone.planos AS p ON p.plano_id = u.plano_id
-  ) AS 'faturamento_medio',
-  (
-    SELECT
-      Round(MIN(p.valor), 2)
-    FROM
-      SpotifyClone.usuarios AS u
-      INNER JOIN SpotifyClone.planos AS p ON p.plano_id = u.plano_id
-  ) AS 'faturamento_minimo',
-  (
-    SELECT
-      round(SUM(p.valor), 2)
-    FROM
-      SpotifyClone.usuarios AS u
-      INNER JOIN SpotifyClone.planos AS p ON p.plano_id = u.plano_id
-  ) AS 'faturamento_total';
+      SpotifyClone.usuarios
+  ) AS A;
