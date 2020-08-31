e294d6f12b816cf1617245056e1b64cbf727a473
Diego Campos <diegodiko05@gmail.com>
2020-08-31 09:41:40 -0300

    Desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,20 +1,17 @@
-USE `SpotifyClone`;
-DROP FUNCTION IF EXISTS `quantidade_musicas_no_historico`;
+USE SpotifyClone;
 DELIMITER $$
 USE `SpotifyClone`$$
-CREATE FUNCTION `quantidade_musicas_no_historico`(pesquisa varchar(10)) RETURNS INT BEGIN
+CREATE FUNCTION quantidade_musicas_no_historico(pesquisa varchar(10)) RETURNS INT READS SQL DATA
+BEGIN
 DECLARE quantidade INT;
 SELECT 
-    COUNT(*)
+    COUNT(*) as `quantidade_musicas_no_historico`
 INTO quantidade FROM
     SpotifyClone.historico_de_reproducoes as hr
     INNER JOIN SpotifyClone.usuarios as u ON u.usuario_id = hr.usuario_id
 WHERE
-
-u.nome
-  = pesquisa;
+  u.nome
+    = pesquisa;
   RETURN quantidade;
 
 END $$
-
-DELIMITER ;
