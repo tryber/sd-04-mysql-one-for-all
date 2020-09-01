3d09b230bcf3f4748e12f2888a299854ff1df630
Ronald Lima <ronaldazevedolima@gmail.com>
2020-09-01 17:24:17 -0300

    E ninguém agora vai me amedrontar

---
+++ desafio2.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+CREATE VIEW estatisticas_musicais AS
+SELECT 
+COUNT(c.id_cancao) AS cancoes,
+(
+SELECT
+COUNT(a.id_artista)
+FROM SpotifyClone.artista AS a
+) AS artistas,
+(
+SELECT
+COUNT(al.id_album)
+FROM SpotifyClone.album AS al
+) AS albuns
+FROM SpotifyClone.cancao AS c;
