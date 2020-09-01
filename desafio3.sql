6b4755beec5436a44ff4dc5961df98a3c218a8d6
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-09-01 08:45:50 -0300

    req3

--- desafio3.sql
+++ desafio3.sql
@@ -1,11 +1,11 @@
 CREATE VIEW historico_reproducao_users AS
 SELECT
   u.name AS usuario,
-  c.name AS name
+  c.name AS nome
 FROM
   SpotifyClone.history AS h
   INNER JOIN SpotifyClone.users AS u ON u.user_id = h.user_id
   INNER JOIN SpotifyClone.songs AS c ON c.cancao_id = h.cancao_id
 ORDER BY
   usuario,
-  name;
+  nome;
