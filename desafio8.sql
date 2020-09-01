0fc7853163c991f30c63aa668f1b5bc7ab522a31
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-01 19:37:41 -0300

    Corrigindo erro parte 2.

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,11 @@
+DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
   BEFORE DELETE ON SpotifyClone.usuario
   FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.artista_usuario AS au
-  WHERE au.usuario_id = OLD.usuario_id;
-  DELETE FROM SpotifyClone.historico_reproducoes AS hr
-  WHERE hr.usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.artista_usuario
+  WHERE usuario_id = OLD.usuario_id;
+  DELETE FROM SpotifyClone.historico_reproducoes
+  WHERE usuario_id = OLD.usuario_id;
 END $$
+DELIMITER ;
