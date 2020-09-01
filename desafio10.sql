c94a87834c1393d51a5f7410de9830d3a29ec71f
Joao Soutto <joaosoutto@hotmail.com>
2020-09-01 19:11:15 -0300

    requisito 10 - mudando de nome pra id

--- desafio10.sql
+++ desafio10.sql
@@ -1,16 +1,32 @@
 USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(usuario VARCHAR(15))
+CREATE FUNCTION quantidade_musicas_no_historico (id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE music_total INT;
-select count(his.usuario_id) AS quantidade_musicas_no_historico
+select count(his.usuario_id)
 from SpotifyClone.historico_de_reproducoes AS his
 INNER JOIN SpotifyClone.usuarios AS usr
 ON his.usuario_id = usr.usuario_id
-WHERE usr.usuario = usuario INTO music_total;
+WHERE usr.usuario_id = id INTO music_total;
 RETURN music_total;
 END $$
 
 DELIMITER ;
+
+-- CREATE FUNCTION quantidade_musicas_no_historico (usuario VARCHAR(15))
+-- RETURNS INT READS SQL DATA
+-- BEGIN
+-- DECLARE music_total INT;
+-- select count(his.usuario_id)
+-- from SpotifyClone.historico_de_reproducoes AS his
+-- INNER JOIN SpotifyClone.usuarios AS usr
+-- ON his.usuario_id = usr.usuario_id
+-- WHERE usr.usuario = usuario INTO music_total;
+-- RETURN music_total;
+-- END $$
+
+-- DELIMITER ;
+
+-- select quantidade_musicas_no_historico('Bill');
