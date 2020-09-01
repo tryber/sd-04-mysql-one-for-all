595500bbfb578fcabfb4cdc5ac1ee4c15e75cad8
henrique <hbaetaleite@gmail.com>
2020-09-01 16:24:55 -0300

    desafio 3 again

--- desafio3.sql
+++ desafio3.sql
@@ -1,9 +1,7 @@
-USE SpotifyClone;
-
 CREATE VIEW historico_reproducao_usuarios as
-SELECT u.user_name AS `usuario`, s.song_name AS `nome` FROM SpotifyClone.user as u
+SELECT u.user_name AS `nome`, s.song_name AS `song` FROM SpotifyClone.user as u
 INNER JOIN SpotifyClone.song_history as sh
 ON u.user_id = sh.user_id
 INNER JOIN SpotifyClone.song as s
 ON s.song_id = sh.song_id
-ORDER BY `usuario`, `nome`;
+ORDER BY `nome`, `song`;
