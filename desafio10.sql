91497b5bf273dd9732158ad806402229782a412c
AliceAAtalla <aliceatalla36@gmail.com>
2020-09-01 17:57:09 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,15 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(nome_usuario VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE qnt_musicas INT;
+SELECT COUNT(*) FROM SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.historico_reproducao_usuarios AS h ON u.id = h.usuario_id
+WHERE nome = nome_usuario INTO qnt_musicas;
+RETURN qnt_musicas;
+END $$
+
+DELIMITER ;
