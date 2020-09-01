e4644d3865832af04be582970af483d779496683
AliceAAtalla <aliceatalla36@gmail.com>
2020-09-01 18:06:30 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+CREATE VIEW cancoes_premium AS
+SELECT
+c.titulo AS nome,
+COUNT(h.cancoes_id) AS reproducoes
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.historico_reproducao_usuarios AS h
+ON c.id = h.cancoes_id
+INNER JOIN SpotifyClone.usuario AS u
+ON u.id = h.usuario_id
+WHERE u.plano_id IN (2, 3)
+GROUP BY c.titulo
+ORDER BY `nome`;
