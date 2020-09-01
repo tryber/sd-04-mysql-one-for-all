d1eb915f1732653bd4d20c1ae7d6fb070fa58c33
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-09-01 18:09:37 -0300

    Requisito 01 - CC fixed

--- desafio1.sql
+++ desafio1.sql
@@ -5,55 +5,55 @@ CREATE DATABASE SpotifyClone;
 USE SpotifyClone;
 
 create table artista (
-	artista_id int primary key auto_increment,
-    nome varchar(100) not null
+artista_id int primary key auto_increment,
+nome varchar(100) not null
 );
 
 create table album (
-	album_id int primary key auto_increment,
-    titulo varchar(100) not null,
-    artista_id int not null,
-    
-    constraint `album_artista_artista_id` FOREIGN KEY(artista_id) references artista(artista_id)
+album_id int primary key auto_increment,
+titulo varchar(100) not null,
+artista_id int not null,
+
+constraint `album_artista_artista_id` FOREIGN KEY(artista_id) references artista(artista_id)
 );
 
 create table cancoes (
-	cancao_id int primary key auto_increment,
-    titulo varchar(100) not null,
-    album_id int not null,
-    
-    constraint `cancoes_album_album_id` foreign key(album_id) references album(album_id)
+cancao_id int primary key auto_increment,
+titulo varchar(100) not null,
+album_id int not null,
+
+constraint `cancoes_album_album_id` foreign key(album_id) references album(album_id)
 );
 
 create table plano (
-	plano_id int primary key auto_increment,
-    titulo varchar(100) not null,
-    valor decimal(10,2) not null
+plano_id int primary key auto_increment,
+titulo varchar(100) not null,
+valor decimal(10,2) not null
 );
 
 create table usuario (
-	usuario_id int primary key auto_increment,
-    nome varchar(100) not null,
-    idade int not null,
-    plano_id int not null,
-    
-    constraint `usuario_plano_id` foreign key (plano_id) references plano(plano_id)    
+usuario_id int primary key auto_increment,
+nome varchar(100) not null,
+idade int not null,
+plano_id int not null,
+
+constraint `usuario_plano_id` foreign key (plano_id) references plano(plano_id)    
 );
 
 create table usuario_segue_artista (
-	id int primary key auto_increment,
-    usuario_id int not null,
-    artista_id int not null,
-    
-    constraint `FK_usuario_id` foreign key (usuario_id) references usuario(usuario_id),
-    constraint `FK_artista_id` foreign key (artista_id) references artista(artista_id)
+id int primary key auto_increment,
+usuario_id int not null,
+artista_id int not null,
+
+constraint `FK_usuario_id` foreign key (usuario_id) references usuario(usuario_id),
+constraint `FK_artista_id` foreign key (artista_id) references artista(artista_id)
 );
 
 create table historico_de_reproducoes (
-	historico_id int primary key auto_increment,
-    usuario_id int not null,
-    cancao_id int not null,
-    
-    constraint `FK_historico_usuario_id` foreign key (usuario_id) references usuario(usuario_id),
-    constraint `FK_cancao_id` foreign key (cancao_id) references cancoes(cancao_id)
+historico_id int primary key auto_increment,
+usuario_id int not null,
+cancao_id int not null,
+
+constraint `FK_historico_usuario_id` foreign key (usuario_id) references usuario(usuario_id),
+constraint `FK_cancao_id` foreign key (cancao_id) references cancoes(cancao_id)
 );
