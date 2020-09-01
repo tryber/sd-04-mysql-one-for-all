7968ea4742401a6b3d8f44158574f454876e482d
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-01 19:30:05 -0300

    Corrigindo erros parte 1.

--- desafio11.sql
+++ desafio11.sql
@@ -1,15 +1,12 @@
 CREATE VIEW cancoes_premium AS
 SELECT
   voice.titulo AS nome,
-  (SELECT COUNT(cancao_id)
-  FROM SpotifyClone.historico_reproducoes AS history
-  WHERE history.cancao_id = voice.cancao_id) AS reproducoes
+  COUNT(*) AS reproducoes
 FROM SpotifyClone.cancao AS voice
 INNER JOIN SpotifyClone.historico_reproducoes AS hr
 ON voice.cancao_id = hr.cancao_id
 INNER JOIN SpotifyClone.usuario AS usr
 ON hr.usuario_id = usr.usuario_id
-INNER JOIN SpotifyClone.plano AS plan
-ON usr.plano_id = plan.plano_id
-WHERE plan.tipo IN ('familiar', 'universitário')
+WHERE usr.plano_id IN (2, 3)
+GROUP BY hr.cancao_id
 ORDER BY nome ASC;
