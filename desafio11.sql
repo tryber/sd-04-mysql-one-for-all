b0dba73e2fed1e924b6af8f7689ddb0246de5395
henrique <hbaetaleite@gmail.com>
2020-09-01 19:38:04 -0300

    codeclimate

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,5 @@
 CREATE VIEW cancoes_premium AS
-SELECT
-	s.song_name AS `nome`,
+SELECT s.song_name AS `nome`,
     COUNT(h.song_id) AS `reproducoes`
 FROM SpotifyClone.song AS s
 INNER JOIN SpotifyClone.song_history AS h
