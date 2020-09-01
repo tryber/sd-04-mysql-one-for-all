6a15dd249492bbb2d56cb03a62906f05a7602ab0
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-01 15:44:27 -0300

    desafio2 concluído.

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+CREATE VIEW estatisticas_musicais AS
+SELECT
+  COUNT(*) AS cancoes,
+  (SELECT COUNT(*) FROM SpotifyClone.artista) AS artistas,
+  (SELECT COUNT(*) FROM SpotifyClone.album) AS albuns
+FROM SpotifyClone.cancao;
