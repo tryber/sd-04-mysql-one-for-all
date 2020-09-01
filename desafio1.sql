8a11c9450ac3ca109f09c99fe6d465e4a891630a
igorgbr <igorgphotoarte@gmail.com>
2020-09-01 18:15:18 -0300

    commita n paga imposto 2 - vai dar erro

--- desafio1.sql
+++ desafio1.sql
@@ -1,13 +1,28 @@
 CREATE DATABASE IF NOT EXISTS SpotifyClone;
+USE SpotifyClone;
 
+CREATE TABLE `planos` (
+  `idplano` int AUTO_INCREMENT,
+  `plano` varchar(45) NOT NULL,
+  `valor_plano` DECIMAL(6,2)NOT NULL,
+  PRIMARY KEY (`idplano`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO
+  planos (plano, valor_plano)
+VALUES
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitario', 5.99);
+  
 CREATE TABLE `usuarios` (
-  `idusuario` int(11) NOT NULL AUTO_INCREMENT,
+  `idusuario` int(11) AUTO_INCREMENT,
   `usuario` varchar(45) NOT NULL,
   `idade` int(11) NOT NULL,
-  `idplano` int(11) NOT NULL,
+  `idplano` int,
   PRIMARY KEY (`idusuario`),
   UNIQUE KEY `idusuario_UNIQUE` (`idusuario`),
-  CONSTRAINT FK_PlanoID FOREIGN KEY (`idplano`)
+  CONSTRAINT  FOREIGN KEY (`idplano`)
   REFERENCES `planos(idplano)`
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -21,10 +36,16 @@ VALUES
 CREATE TABLE `artistas` (
   `idartista` int(11) NOT NULL AUTO_INCREMENT,
   `artista` varchar(45) NOT NULL,
-  `idusuario` int(11),
   PRIMARY KEY (`idartista`),
   UNIQUE KEY `idartista_UNIQUE` (`idartista`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO artistas(artista)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
 
 
 CREATE TABLE `seguindo_artista` (
@@ -37,6 +58,17 @@ CREATE TABLE `seguindo_artista` (
   REFERENCES `usuario(idusuario)`
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO
+  seguindo_artista (idusuario, idartista)
+VALUES
+  (1, 1),
+  (1, 2),
+  (1, 3),
+  (2, 1),
+  (2, 2),
+  (2, 3),
+  (3, 1);
+
 CREATE TABLE `historico` (
   `idusuario` int(11) NOT NULL,
   `idcancao` int(11),
@@ -47,32 +79,68 @@ CREATE TABLE `historico` (
   REFERENCES `cancoes(idcancao)`
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `planos` (
-  `idplano` int(11) NOT NULL AUTO_INCREMENT,
-  `plano` varchar(45) NOT NULL,
-  `valor_plano` DECIMAL(6,2)NOT NULL,
-  PRIMARY KEY (`idplano`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO
+  historico (idusuario, idcancao)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
+  (2, 2);
+
 
 CREATE TABLE `albuns` (
   `idalbum` int(11) NOT NULL AUTO_INCREMENT,
-  `album` int(11),
-  `idartista` int(11),
+  `album` VARCHAR(300) NOT NULL,
+  `idartista` int(11) NOT NULL,
   PRIMARY KEY (`idalbum`),
   CONSTRAINT FK_ArtistaID FOREIGN KEY (`idartista`)
   REFERENCES `artista(idartista)`
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO
+  albuns (album, idartista)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+
 CREATE TABLE `cancoes` (
   `idcancao` int(11) NOT NULL AUTO_INCREMENT,
-  `cancao` int(11),
-  `idartista` int(11),
-  `idalbum` int(11),
+  `cancao` VARCHAR(300) NOT NULL,
+  `idartista` int(11) NOT NULL,
+  `idalbum` int(11) NOT NULL,
   PRIMARY KEY (`idcancao`),
   CONSTRAINT FK_AlbumID FOREIGN KEY (`idartista`)
   REFERENCES `artistas(idartista)`,
   CONSTRAINT FK_AlbumID FOREIGN KEY (`idalbum`)
   REFERENCES `albuns(idalbum)`
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO
+  cancoes (cancao, idalbum, idartista)
+VALUES
+  ('Soul For Us', 1, 1),
+  ('Reflections Of Magic', 1, 1),
+  ('Dance With Her Own', 1, 1),
+  ('Troubles Of My Inner Fire', 2, 1),
+  ('Time Fireworks', 2, 1),
+  ('Magic Circus', 3, 2),
+  ('Honey, So Do I', 3, 2),
+  ("Sweetie, Let's Go Wild", 3, 2),
+  ('She Knows', 3, 2),
+  ('Fantasy For Me', 4, 3),
+  ('Celebration Of More', 4, 3),
+  ('Rock His Everything', 4, 3),
+  ('Home Forever', 4, 3),
+  ('Diamond Power', 4, 3),
+  ("Honey, Let's Be Silly", 4, 3),
+  ('Thang Of Thunder', 5, 4),
+  ('Words Of Her Life', 5, 4),
+  ('Without My Streets', 5, 4);
 
 USE SpotifyClone;
