9763cd80b3681df161ad41fbdca391e518a3620e
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-09-01 00:00:21 -0300

    Montagem da tabela

--- desafio1.sql
+++ desafio1.sql
@@ -1,16 +1,88 @@
-{
-  "coluna_usuario": "nome-da-coluna",
-  "tabela_que_contem_usuario": "nome-da-tabela-que-armazena-a-coluna",
-  "coluna_plano": "nome-da-coluna",
-  "tabela_que_contem_plano": "nome-da-tabela-que-armazena-a-coluna",
-  "coluna_historico_de_reproducoes": "nome-da-coluna",
-  "tabela_que_contem_historico_de_reproducoes": "nome-da-tabela-que-armazena-a-coluna",
-  "coluna_seguindo_artistas": "nome-da-coluna",
-  "tabela_que_contem_seguindo_artistas": "nome-da-tabela-que-armazena-a-coluna",
-  "coluna_artista": "nome-da-coluna",
-  "tabela_que_contem_artista": "nome-da-tabela-que-armazena-a-coluna",
-  "coluna_album": "nome-da-coluna",
-  "tabela_que_contem_album": "nome-da-tabela-que-armazena-a-coluna",
-  "coluna_cancoes": "nome-da-coluna",
-  "tabela_que_contem_cancoes": "nome-da-tabela-que-armazena-a-coluna"
-}
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE plan(
+    plan_id INT PRIMARY KEY AUTO_INCREMENT,
+    plan_name VARCHAR(50) NOT NULL,
+    plan_price DECIMAL(3, 2) NOT NULL
+) engine = InnoDB;
+
+CREATE TABLE user(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(100) NOT NULL,
+    user_age INT NOT NULL,
+    plan_id INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (plan_id) REFERENCES plan(plan_id)
+) engine = InnoDB;
+
+CREATE TABLE song(
+    song_id INT PRIMARY KEY AUTO_INCREMENT,
+    song_name VARCHAR(100) NOT NULL    
+) engine = InnoDB;
+
+CREATE TABLE artist(
+    artist_id INT PRIMARY KEY AUTO_INCREMENT,
+    artist_name VARCHAR(100) NOT NULL    
+) engine = InnoDB;
+
+CREATE TABLE album(
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album_name VARCHAR(100) NOT NULL,
+    artist_id INT NOT NULL,
+    song_id INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
+    CONSTRAINT FOREIGN KEY (song_id) REFERENCES song(song_id)
+) engine = InnoDB;
+
+CREATE TABLE user_preferences(
+    user_id INT NOT NULL,
+    artist_id INT NOT NULL,
+    song_id INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CONSTRAINT FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
+    CONSTRAINT FOREIGN KEY (song_id) REFERENCES song(song_id)
+) engine = InnoDB;
+
+INSERT INTO plan (plan_name, plan_price)
+VALUES
+  ('gratuito', 0),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+
+INSERT INTO user (user_name, user_age, plan_id)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);  
+
+INSERT INTO song (song_name)
+VALUES
+	('Soul For Us'),
+    ('Reflections Of Magic'),
+    ('Dance With Her Own'),
+    ('Troubles Of My Inner Fire'),
+    ('Time Fireworks'),
+    ('Magic Circus'),
+    ('Honey, So Do I'),
+    ('Sweetie, Let’s Go Wild'),
+    ('She Knows'),
+    ('Fantasy For Me'),
+    ('Celebration Of More'),
+    ('Rock His Everything'),
+    ('Home Forever'),
+    ('Diamond Powe'),
+    ('Honey, Let`s Be Silly'),
+    ('Thang Of Thunder'),
+    ('Words Of Her Life'),
+    ('Without My Streets');
+    
+INSERT INTO artist (artist_name)
+VALUES
+	('Walter Phoenix'),
+    ('Peter Strong'),
+    ('Lance Day'),
+    ('Freedie Shannon');
