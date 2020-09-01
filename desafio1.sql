83bbce8e5bb3103719de68596d6be02343d6f893
Ronald Lima <ronaldazevedolima@gmail.com>
2020-09-01 18:12:40 -0300

    E a imaginação me dá forças pra voar

--- desafio1.sql
+++ desafio1.sql
@@ -52,7 +52,7 @@ VALUES
 ('Soul For Us', 1),
 ('Reflections Of Magic', 1),
 ('Dance With Her Own', 1),
-('Trouble Of My Inner Fire', 2),
+('Troubles Of My Inner Fire', 2),
 ('Time Fireworks', 2),
 ('Magic Circus', 3),
 ('Honey, So Do I', 3),
@@ -63,7 +63,7 @@ VALUES
 ('Rock His Everything', 4),
 ('Home Forever', 4),
 ('Diamond Power', 4),
-('Honey, Let`s Be Silly', 4),
+("Honey, Let's Be Silly", 4),
 ('Thang Of Thunder', 5),
 ('Words Of Her Life', 5),
 ('Without My Streets', 5);
