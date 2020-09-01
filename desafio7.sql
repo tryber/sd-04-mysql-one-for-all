2a170fba76518cc414b35b02b1b8a9af4ebc942f
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-09-01 18:03:25 -0300

    desafio7

--- desafio7.sql
+++ desafio7.sql
@@ -7,4 +7,3 @@ FROM
     SpotifyClone.artistas AS A INNER JOIN SpotifyClone.albuns AS AL ON A.artista_id = AL.artista_id
     INNER JOIN SpotifyClone.seguindo_artista AS S ON S.artista_id = A.artista_id GROUP BY album_id
     ORDER BY 3 DESC, 1, 2;
-    
