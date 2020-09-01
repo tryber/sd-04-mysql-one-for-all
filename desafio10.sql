aecbb3db5b8e4cd2aca276ac12d7a9e1d0db0e56
Joao Soutto <joaosoutto@hotmail.com>
2020-09-01 19:17:54 -0300

    uai

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,7 @@
 USE SpotifyClone;
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico (id INT)
+CREATE FUNCTION quantidade_musicas_no_historico(usuario_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE music_total INT;
@@ -9,7 +9,7 @@ select count(his.usuario_id)
 from SpotifyClone.historico_de_reproducoes AS his
 INNER JOIN SpotifyClone.usuarios AS usr
 ON his.usuario_id = usr.usuario_id
-WHERE usr.usuario_id = id INTO music_total;
+WHERE usr.usuario_id = usuario_id INTO music_total;
 RETURN music_total;
 END $$
 
