60be59e17e375371cac50273190a9245c5f548da
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-09-01 20:02:23 -0300

    desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -3,22 +3,16 @@ USE SpotifyClone;
 DROP VIEW IF EXISTS estatisticas_musicais;
 
 CREATE VIEW estatisticas_musicais AS
-SELECT
-  (
-    SELECT
-      COUNT(*)
-    FROM
-      cancoes
-  ) AS cancoes,
-  (
-    SELECT
-      COUNT(*)
-    FROM
-      artistas
-  ) AS artistas,
-  (
-    SELECT
-      COUNT(*)
-    FROM
-      albuns
-  ) AS albuns;
+    SELECT 
+        (SELECT 
+                COUNT(*)
+            FROM
+                cancoes) AS cancoes,
+        (SELECT 
+                COUNT(*)
+            FROM
+                artistas) AS artistas,
+        (SELECT 
+                COUNT(*)
+            FROM
+                albuns) AS albuns;
