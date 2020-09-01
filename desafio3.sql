83ddd726a31b386cc26c597fe8114088c91d6646
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-09-01 20:19:01 -0300

    desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,15 +1,16 @@
 USE SpotifyClone;
 
-DROP VIEW IF EXISTS historico_repoducao_usuarios;
+DROP VIEW IF EXISTS historico_reproducao_usuarios;
 
-CREATE VIEW historico_reproducao_usuarios AS
-SELECT
-  u.nome AS usuario,
-  c.nome AS nome
-FROM
-  usuarios_cancoes AS uc
-  INNER JOIN usuarios AS u ON (u.id = uc.usuario_id)
-  INNER JOIN cancoes AS c ON (c.id = uc.cancao_id)
-ORDER BY
-  usuario,
-  nome;
+CREATE VIEW historico_reproducao_usuarios AS (
+  SELECT
+    u.nome AS usuario,
+    c.nome AS nome
+  FROM
+    usuarios_cancoes AS uc
+    INNER JOIN usuarios AS u ON (u.id = uc.usuario_id)
+    INNER JOIN cancoes AS c ON (c.id = uc.cancao_id)
+  ORDER BY
+    usuario,
+    nome
+);
