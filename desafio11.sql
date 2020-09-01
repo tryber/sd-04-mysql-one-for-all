67a658f79e591396dc9c2e3c8eaa2e7dbae35f45
henrique <hbaetaleite@gmail.com>
2020-09-01 19:16:08 -0300

    desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,11 @@
 CREATE VIEW cancoes_premium AS
 SELECT s.song_name AS `nome`,
-COUNT(DISTINCT h.song_id) AS `reproducoes`
+COUNT(h.song_id) AS `reproducoes`
 FROM SpotifyClone.song AS s
 INNER JOIN SpotifyClone.song_history AS h
 ON s.song_id = h.song_id
--- WHERE plan_name IN('familiar', 'universitario')
+INNER JOIN SpotifyClone.user AS us
+ON h.user_id = us.user_id
+AND us.plan_id IN(2, 3)
 GROUP BY `nome`
 ORDER BY `nome`;
