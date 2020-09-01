391f519bae436871d9d1c3d06be50c963134263c
henrique <hbaetaleite@gmail.com>
2020-09-01 00:31:38 -0300

    continuando desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -18,11 +18,6 @@ CREATE TABLE user(
     CONSTRAINT FOREIGN KEY (plan_id) REFERENCES plan(plan_id)
 ) engine = InnoDB;
 
-CREATE TABLE song(
-    song_id INT PRIMARY KEY AUTO_INCREMENT,
-    song_name VARCHAR(100) NOT NULL    
-) engine = InnoDB;
-
 CREATE TABLE artist(
     artist_id INT PRIMARY KEY AUTO_INCREMENT,
     artist_name VARCHAR(100) NOT NULL    
@@ -31,18 +26,31 @@ CREATE TABLE artist(
 CREATE TABLE album(
     album_id INT PRIMARY KEY AUTO_INCREMENT,
     album_name VARCHAR(100) NOT NULL,
-    artist_id INT NOT NULL,
-    song_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
-    CONSTRAINT FOREIGN KEY (song_id) REFERENCES song(song_id)
+    artist_id INT NOT NULL,    
+    CONSTRAINT FOREIGN KEY (artist_id) REFERENCES artist(artist_id)    
 ) engine = InnoDB;
 
-CREATE TABLE user_preferences(
+
+CREATE TABLE song(
+    song_id INT PRIMARY KEY AUTO_INCREMENT,
+    song_name VARCHAR(100) NOT NULL,
+    album_id INT NOT NULL,       
+    CONSTRAINT FOREIGN KEY (album_id) REFERENCES album(album_id)    
+) engine = InnoDB;
+
+CREATE TABLE followed_artist(
     user_id INT NOT NULL,
     artist_id INT NOT NULL,
-    song_id INT NOT NULL,
+    PRIMARY KEY (`user_id`,`artist_id`),
     CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(user_id),
-    CONSTRAINT FOREIGN KEY (artist_id) REFERENCES artist(artist_id),
+    CONSTRAINT FOREIGN KEY (artist_id) REFERENCES artist(artist_id)    
+) engine = InnoDB;
+
+CREATE TABLE song_history(
+	user_id INT NOT NULL,
+    song_id INT NOT NULL,
+    PRIMARY KEY (`user_id`,`song_id`),
+    CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(user_id),
     CONSTRAINT FOREIGN KEY (song_id) REFERENCES song(song_id)
 ) engine = InnoDB;
 
@@ -57,32 +65,71 @@ VALUES
   ('Thati', 23, 1),
   ('Cintia', 35, 2),
   ('Bill', 20, 3),
-  ('Roger', 45, 1);  
-
-INSERT INTO song (song_name)
-VALUES
-('Soul For Us'),
-    ('Reflections Of Magic'),
-    ('Dance With Her Own'),
-    ('Troubles Of My Inner Fire'),
-    ('Time Fireworks'),
-    ('Magic Circus'),
-    ('Honey, So Do I'),
-    ('Sweetie, Let’s Go Wild'),
-    ('She Knows'),
-    ('Fantasy For Me'),
-    ('Celebration Of More'),
-    ('Rock His Everything'),
-    ('Home Forever'),
-    ('Diamond Powe'),
-    ('Honey, Let`s Be Silly'),
-    ('Thang Of Thunder'),
-    ('Words Of Her Life'),
-    ('Without My Streets');
-    
+  ('Roger', 45, 1);
+  
 INSERT INTO artist (artist_name)
 VALUES
-('Walter Phoenix'),
+	('Walter Phoenix'),
     ('Peter Strong'),
     ('Lance Day'),
     ('Freedie Shannon');
+
+INSERT INTO album (album_name, artist_id)
+VALUES
+	('Envious', 1),
+    ('Exuberant', 1),
+    ('Hallowed Steam', 2),
+    ('Incandescent', 3),
+    ('Temporary Culture', 4);
+
+
+INSERT INTO song (song_name)
+VALUES
+	('Soul For Us', 1),
+    ('Reflections Of Magic', 1),
+    ('Dance With Her Own', 1),
+    ('Troubles Of My Inner Fire', 2),
+    ('Time Fireworks', 2),
+    ('Magic Circus', 3),
+    ('Honey, So Do I', 3),
+    ('Sweetie, Let’s Go Wild', 3),
+    ('She Knows', 3),
+    ('Fantasy For Me', 4),
+    ('Celebration Of More', 4),
+    ('Rock His Everything', 4),
+    ('Home Forever', 4),
+    ('Diamond Powe', 4),
+    ('Honey, Let`s Be Silly', 4),
+    ('Thang Of Thunder', 5),
+    ('Words Of Her Life', 5),
+    ('Without My Streets', 5);
+    
+    
+    INSERT INTO followed_artist (user_id, artist_id)
+    VALUES
+		(1, 1),
+        (1, 4),
+        (1, 3),
+        (2, 1),
+        (2, 3),
+        (3, 2),
+        (3, 1),
+        (4, 4);
+
+
+    INSERT INTO song_history (user_id, song_id)
+    VALUE
+		(1, 1),
+        (1, 6),
+        (1, 14),
+        (1, 16),
+        (2, 13),
+        (2, 17),
+        (2, 2),
+        (2, 15),
+        (3, 4),
+        (3, 16),
+        (3, 6),
+        (4, 3),
+        (4, 18),
+        (4, 11);
