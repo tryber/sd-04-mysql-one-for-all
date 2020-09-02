7b169fb7c530ee785dcbe0f35e94d5895d81ecee
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-09-02 14:14:02 -0300

    cc 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,13 +1,12 @@
-DELIMITER $$
-CREATE TRIGGER trigger_usuario_delete
-	BEFORE DELETE ON usuario FOR EACH ROW
-BEGIN
-	DELETE FROM historico_reproducao 
+DELIMITER $$ CREATE TRIGGER trigger_usuario_delete BEFORE DELETE ON usuario FOR EACH ROW BEGIN
+DELETE FROM historico_reproducao 
 WHERE
     usuario_id = OLD.id;
+
 DELETE FROM artista_seguido 
 WHERE
     usuario_id = OLD.id;
-    END; $$
-    DELIMITER ;
-    SET SQL_SAFE_UPDATES = 0;
+
+END;
+
+$$ DELIMITER;
