502510eab7f46e1b99f15bc602e5724fa695c3cf
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-09-02 11:16:59 -0300

    evaluator fixes

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
 CREATE VIEW cancoes_premium AS
-SELECT c.cancao, count(*) FROM usuarios u 
+SELECT c.cancao AS nome, count(*) AS reproducoes FROM usuarios u 
 INNER JOIN usuarios_cancoes uc ON uc.id_usuario = u.id_usuario 
 INNER JOIN cancoes c ON uc.id_cancao = c.id_cancao
 WHERE u.id_plano IN(2,3)
