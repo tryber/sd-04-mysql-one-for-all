b270b76eabfb09777a228e8a8afd5933d669ef97
Ronald Lima <ronaldazevedolima@gmail.com>
2020-09-02 19:16:09 -0300

    Dragon ball z meu compromisso é sempre vencer

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
-DELETE FROM SpotifyClone.musica_usuario WHERE usuario.id_usuario = musica_usuario.id_usuario;
-DELETE FROM SpotifyClone.artista_usuario WHERE usuario.id_usuario = artista_usuario.id_usuario;
+DELETE FROM SpotifyClone.musica_usuario WHERE OLD.id_usuario = musica_usuario.id_usuario;
+DELETE FROM SpotifyClone.artista_usuario WHERE OLD.id_usuario = artista_usuario.id_usuario;
 END; $$
 DELIMITER ;
