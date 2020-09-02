84fa512dc2d2914bd1c497fe32c76f17430b68ae
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-09-02 18:41:40 -0300

    8

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,15 @@
-USE SpotifyClone;
+-- USE SpotifyClone;
 
-DROP TRIGGER IF EXISTS trigger_usuario_delete;
-
-CREATE TRIGGER trigger_usuario_delete BEFORE DELETE ON usuarios FOR each ROW
+-- DROP TRIGGER IF EXISTS trigger_usuario_delete;
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete 
+BEFORE DELETE ON usuarios 
+FOR EACH ROW 
+BEGIN  
+DELETE FROM usuarios_artistas 
+where usuario_id = old.id;
+DELETE FROM usuarios_cancoes 
+WHERE
+  usuario_id = old.id;
+  END;
+$$ DELIMITER ;
