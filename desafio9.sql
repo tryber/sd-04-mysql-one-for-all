e5c1be3f4358e72bdb2e91886476beb783444757
Orlando <programadorlando@gmail.com>
2020-09-02 09:29:42 -0300

    desafio 9 + CC issues

--- desafio9.sql
+++ desafio9.sql
@@ -3,7 +3,7 @@ USE SpotifyClone;
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(50))
 BEGIN
-	SELECT artists.name AS artista, albums.name AS album FROM albums
+  SELECT artists.name AS artista, albums.name AS album FROM albums
     INNER JOIN artists
     ON albums.artist_id = artists.artist_id
     WHERE artists.name = artist_name
