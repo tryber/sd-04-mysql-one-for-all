3f1a2b78b84bc466c3a8fe3bb3f0bf4181fe71a6
Orlando <programadorlando@gmail.com>
2020-09-01 21:43:28 -0300

    desafio 5 correcoes

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,6 @@
-USE SpotifyClone;
 CREATE VIEW top_2_hits_do_momento AS
   SELECT songs.name AS cancao, COUNT(*) AS reproducoes FROM songs
   INNER JOIN playback_history as playback_history
-  WHERE songs.song_id = playback_history.song_id
+  ON songs.song_id = playback_history.song_id
   GROUP BY songs.song_id
   ORDER BY reproducoes DESC, cancao LIMIT 2;
