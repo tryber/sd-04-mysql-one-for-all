7793a8d0437fb4a08a73aaa4dafb5a892df20d51
Victor L <victor.lustosacp@gmail.com>
2020-09-02 12:02:30 -0300

    erro

--- desafio2.sql
+++ desafio2.sql
@@ -2,5 +2,5 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT count(DISTINCT c.cancoes_name) AS cancoes ,
 count(DISTINCT a.artistas_name) AS artistas ,
-count(DISTINCT al.album_name) AS albums
+count(DISTINCT al.album_name) AS albuns
 FROM SpotifyClone.cancoes as c, SpotifyClone.artistas  as a, SpotifyClone.album  as al;
