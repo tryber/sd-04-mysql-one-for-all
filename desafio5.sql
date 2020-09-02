799c3d86059457dae3763b3b6152860850df22e7
Victor L <victor.lustosacp@gmail.com>
2020-09-02 13:20:26 -0300

    Desafio5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT c.cancoes_name AS cancao, COUNT(h.id_cancoes) AS reproducoes
+FROM SpotifyClone.cancoes AS c
+INNER JOIN SpotifyClone.historico AS h
+ON c.cancoes_id = h.id_cancoes
+GROUP BY cancao
+order by reproducoes desc
+LIMIT 2;
