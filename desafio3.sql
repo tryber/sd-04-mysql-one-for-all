6b2baa5f0dc00d762af4cdd0c65cff3959e9df79
Victor L <victor.lustosacp@gmail.com>
2020-09-02 12:46:31 -0300

    cc

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT u.usuario_name as `usuario`, c.cancoes_name as `nome`
 from SpotifyClone.usuario AS u
-INNER JOIN SpotifyClone.cancoes AS chistorico_reproducao_usuarios
+INNER JOIN SpotifyClone.cancoes AS c
 INNER JOIN SpotifyClone.historico AS h
 WHERE u.usuario_id = h.id_usuario AND c.cancoes_id = h.id_cancoes
 ORDER BY `usuario`, `nome`;
