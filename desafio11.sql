d7f8dc7db15f290123790aef94d1f1b51ae6c283
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-09-01 21:46:40 -0300

    agora sim é uma view (o commit anterior nunca existiu)

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,13 @@
-SELECT 
-    s.name AS nome,
-    COUNT(*) AS reproducoes
-FROM
-    SpotifyClone.song AS s
-        JOIN
-    SpotifyClone.playback_history AS play ON play.id_song = s.id
-        JOIN
+CREATE VIEW cancoes_premium AS
+    SELECT 
+        s.name AS nome, COUNT(*) AS reproducoes
+    FROM
+        SpotifyClone.song AS s
+            JOIN
+        SpotifyClone.playback_history AS play ON play.id_song = s.id
+            JOIN
 -- SELECIONA PARA CONTAGEM SOMENTE AS REPRODUÇÕES DE PLANOS NÃO GRATUITOS (ID do plano gratuito === 1)
-    SpotifyClone.user AS u ON u.id = play.id_user AND u.id_plan <> 1
-GROUP BY nome
-ORDER BY nome;
+        SpotifyClone.user AS u ON u.id = play.id_user AND u.id_plan <> 1
+    GROUP BY nome
+    ORDER BY nome;
+-- END OF VIEW
