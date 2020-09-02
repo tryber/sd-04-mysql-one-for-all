9c8e502ccf9f09bf0d00de643288c3c5d18a8143
Victor L <victor.lustosacp@gmail.com>
2020-09-02 13:11:28 -0300

    4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,8 @@
 CREATE VIEW top_3_artistas AS
-SELECT a.artistas_name AS artista, COUNT(s.id_usuario) AS seguidores
-FROM SpotifyClone.artistas AS a
-INNER JOIN SpotifyClone.seguindo_artistas AS s
+SELECT a.artistas_name artista, COUNT(s.id_usuario) seguidores
+FROM SpotifyClone.artistas a
+INNER JOIN SpotifyClone.seguindo_artistas s
 ON a.artistas_id = s.id_artistas
 GROUP BY artista
 ORDER BY seguidores DESC, artista
 LIMIT 3;
-
-SELECT * FROM top_3_artistas;
