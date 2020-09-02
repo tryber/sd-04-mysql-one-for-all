bff67143fbe1a47000e70d4fb55610af7341be0c
procopio420 <lucas-procopio@outlook.com>
2020-09-01 22:29:38 -0300

    fuckup.

--- desafio3.sql
+++ desafio3.sql
@@ -1,16 +1,11 @@
-USE SpotifyClone;
-
-DROP VIEW historico_reproducao_usuarios;
-
-CREATE VIEW historico_reproducao_usuarios AS
-    SELECT 
-        usuario.nome AS usuario, cancoes.nome AS nome
-    FROM
-        SpotifyClone.usuarios_cancoes AS historico
-            INNER JOIN
-        SpotifyClone.usuarios AS usuario ON usuario.id = historico.usuario_id
-            INNER JOIN
-        SpotifyClone.cancoes AS cancoes ON cancoes.id = historico.cancao_id
-        ORDER BY usuario, nome;
-
-SELECT * FROM historico_reproducao_usuarios;
+CREATE VIEW historico_reproducao_usuarios
+AS
+  SELECT
+    usuario.nome AS usuario, cancoes.nome AS nome
+  FROM
+    SpotifyClone.usuarios_cancoes AS historico
+    INNER JOIN
+    SpotifyClone.usuarios AS usuario ON usuario.id = historico.usuario_id
+    INNER JOIN
+    SpotifyClone.cancoes AS cancoes ON cancoes.id = historico.cancao_id
+  ORDER BY usuario, nome;
