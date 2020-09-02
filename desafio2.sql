b17726cd3ce995cd1f149a960bfbbdf04f432485
procopio420 <lucas-procopio@outlook.com>
2020-09-01 21:28:24 -0300

    Igor broke the regex, lame

--- desafio2.sql
+++ desafio2.sql
@@ -1,11 +1,10 @@
 USE SpotifyClone;
 
-CREATE VIEW estatisticas_musicais AS
-    (SELECT USE SpotifyClone;
-
 DROP VIEW estatisticas_musicais;
 
-CREATE VIEW estatisticas_musicais AS (
+CREATE VIEW estatisticas_musicais
+AS
+  (
   SELECT
     (SELECT
       COUNT(*)
@@ -21,16 +20,19 @@ CREATE VIEW estatisticas_musicais AS (
       SpotifyClone.albuns) AS albuns
 );
 
-        (SELECT 
-                COUNT(*)
-            FROM
-                SpotifyClone.cancoes) AS cancoes,
-        (SELECT 
-                COUNT(*)
-            FROM
-                SpotifyClone.artistas) AS artistas,
-        (SELECT 
-                COUNT(*)
-            FROM
-                SpotifyClone.albuns) AS albuns
+(SELECT
+  COUNT(*)
+FROM
+  SpotifyClone.cancoes)
+AS cancoes,
+(SELECT
+  COUNT(*)
+FROM
+  SpotifyClone.artistas)
+AS artistas,
+(SELECT
+  COUNT(*)
+FROM
+  SpotifyClone.albuns)
+AS albuns
     );
