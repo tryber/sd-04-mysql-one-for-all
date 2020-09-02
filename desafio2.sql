df9d390bd3f766cbd6c96e4177af7a7eb1d08ca9
procopio420 <lucas-procopio@outlook.com>
2020-09-01 22:25:45 -0300

    It's getting hard to keep up with the crap I've trashed

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,11 @@
-CREATE VIEW estatisticas_musicais AS (
+CREATE VIEW estatisticas_musicais
+AS
+  (
   SELECT
-    (SELECT COUNT(id) FROM SpotifyClone.cancoes) AS cancoes,
-    (SELECT COUNT(id) FROM SpotifyClone.artistas) AS artistas,
-    (SELECT COUNT(id) FROM SpotifyClone.albuns) AS albuns
+    (SELECT COUNT(id)
+    FROM SpotifyClone.cancoes) AS cancoes,
+    (SELECT COUNT(id)
+    FROM SpotifyClone.artistas) AS artistas,
+    (SELECT COUNT(id)
+    FROM SpotifyClone.albuns) AS albuns
 );
