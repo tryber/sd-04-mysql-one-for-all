e9e3995c662dd322d6ee48659f23a630c95573eb
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-09-02 11:15:17 -0300

    desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,20 @@
+-- USE SpotifyClone;
+-- DROP VIEW IF EXISTS top_2_hits_do_momento;
+-- CREATE VIEW top_2_hits_do_momento AS
+SELECT
+  c.nome AS cancao,
+  (
+    SELECT
+      count(*)
+    FROM
+      usuarios_cancoes AS uc
+    WHERE
+      uc.cancao_id = c.id
+  ) AS reproducoes
+FROM
+  cancoes AS c
+ORDER BY
+  reproducoes DESC,
+  cancao
+LIMIT
+  2;
