f7879eef1a844b5d25c55e33e69e97edf632fb2a
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-09-02 10:50:50 -0300

    matheusão

--- desafio5.sql
+++ desafio5.sql
@@ -1,17 +1,1 @@
-USE SpotifyClone;
-CREATE VIEW estatisticas_musicais
-AS
-  (SELECT
-    (SELECT
-      COUNT(*)
-    FROM
-      cancoes) AS cancoes,
-    (SELECT
-      COUNT(*)
-    FROM
-      artistas) AS artistas,
-    (SELECT
-      COUNT(*)
-    FROM
-      albuns) AS albuns
-    );
+
