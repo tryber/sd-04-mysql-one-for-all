88019d775f5e963083cffd69cb355740d902d976
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-09-02 19:44:06 -0300

    11

---
+++ desafio11.sql
@@ -0,0 +1,22 @@
+-- USE SpotifyClone;
+-- DROP VIEW IF EXISTS cancoes_premium;
+CREATE VIEW cancoes_premium AS
+SELECT
+  c.nome AS nome,
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      usuarios_cancoes AS uc
+      INNER JOIN usuarios AS u ON (u.id = uc.usuario_id)
+    WHERE
+      uc.cancao_id = c.id
+      AND u.plano_id IN (2, 3)
+  ) AS reproducoes
+FROM
+  cancoes AS c
+GROUP BY
+  c.id
+ORDER BY
+  reproducoes DESC,
+  nome;
