f3b29ebb7d53e70fc5bd0def200fd8996d2a4049
procopio420 <lucas-procopio@outlook.com>
2020-09-01 21:46:37 -0300

    Please forgive me

--- desafio2.sql
+++ desafio2.sql
@@ -1,19 +1,17 @@
 USE SpotifyClone;
-
 CREATE VIEW estatisticas_musicais
 AS
-  (
-  SELECT
+  (SELECT
     (SELECT
       COUNT(*)
     FROM
-      SpotifyClone.cancoes) AS cancoes,
+      cancoes) AS cancoes,
     (SELECT
       COUNT(*)
     FROM
-      SpotifyClone.artistas) AS artistas,
+      artistas) AS artistas,
     (SELECT
       COUNT(*)
     FROM
-      SpotifyClone.albuns) AS albuns
-);
+      albuns) AS albuns
+    );
