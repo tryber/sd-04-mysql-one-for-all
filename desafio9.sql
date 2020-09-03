a4505cefcc60a06f88768f469f8aafcc92a74e54
henrique <hbaetaleite@gmail.com>
2020-09-03 16:34:43 -0300

    desafio 9 again

--- desafio9.sql
+++ desafio9.sql
@@ -3,10 +3,11 @@ DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN artistName VARCHAR(100))
 BEGIN
 SELECT
-ar.artist_name AS artista,
-al.album_name AS album
+ar.artist_name AS `artista`,
+al.album_name AS `album`
 FROM SpotifyClone.album AS al
-JOIN SpotifyClone.artist AS ar ON ar.id = al.artist_id AND artistName = ar.artist_name
+JOIN SpotifyClone.artist AS ar
+ON ar.artist_id = al.artist_id AND artistName = ar.artist_name
 ORDER BY album;
 END $$
 
