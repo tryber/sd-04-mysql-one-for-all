bc8101afad7bdfc74163d278f9312fd1060b2049
henrique <hbaetaleite@gmail.com>
2020-09-03 18:34:14 -0300

    desafio8

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,9 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.user
 FOR EACH ROW
 BEGIN
-  DELETE FROM SpotifyClone.song_history WHERE user_id = OLD.user_id
-  DELETE FROM SpotifyClone.followed_artist WHERE user_id = OLD.user_id
+  DELETE FROM SpotifyClone.song_history
+    WHERE SpotifyClone.song_history.user_id = OLD.id
+  DELETE FROM SpotifyClone.followed_artist
+    WHERE SpotifyClone.followed_artist.user_id = OLD.id
 END; $$
 DELIMITER ;
