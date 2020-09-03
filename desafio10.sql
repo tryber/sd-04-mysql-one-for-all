9ceda4856139d73a9dd95531e504583e8edadcbd
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-03 02:47:02 -0300

    Fix

--- desafio10.sql
+++ desafio10.sql
@@ -4,8 +4,11 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(userID INT)
 RETURNS INT READS SQL DATA
 BEGIN
+DECLARE result INT;
 SELECT COUNT(song_id) AS quantidade_musicas_no_historico FROM Songs_History
-WHERE user_id = userID;
+WHERE user_id = userID
+INTO result;
+RETURN result;
 END $$
 
 DELIMITER ;
