c7cb6346e89d845b136d36d7d055d2ef1a19c2ea
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-09-03 02:39:29 -0300

    desafio 11

--- desafio10.sql
+++ desafio10.sql
@@ -2,6 +2,7 @@ USE SpotifyClone;
 DELIMITER $$
 
 CREATE FUNCTION quantidade_musicas_no_historico(userID INT)
+RETURNS INT READS SQL DATA
 BEGIN
 SELECT COUNT(song_id) AS quantidade_musicas_no_historico FROM Songs_History
 WHERE user_id = userID;
