f2bf78882944f806ceac80024193f666a0ddffe5
Julio Cesar <stininmatic@hotmail.com>
2020-09-03 20:25:09 -0300

    Vamos ver no que da

--- desafio1.sql
+++ desafio1.sql
@@ -58,73 +58,72 @@ foreign key (usuario_id) references usuarios(usuario_id)
 )engine=InnoDB;
 
 insert into artistas (artista_id, artista)
-	values
-	(null, "Walter Phoenix"),
-	(null, "Freedie Shannon"),
-	(null, "Lance Day"),
-	(null, "Peter Strong");
+values
+(null, "Walter Phoenix"),
+(null, "Freedie Shannon"),
+(null, "Lance Day"),
+(null, "Peter Strong");
 
 insert into albuns (album_id, album, artista_id)
-	values
-	(1, "Envious", 1),
-	(null, "Exuberant", 1),
-	(null, "Hallowed Steam", 4),
-	(null, "Incandescent", 3),
-	(null, "Temporary Culture", 2);
+values
+(1, "Envious", 1),
+(null, "Exuberant", 1),
+(null, "Hallowed Steam", 4),
+(null, "Incandescent", 3),
+(null, "Temporary Culture", 2);
 
 insert into musicas (musica_id, nome_musica, album_id)
-	values
-	(1, "Soul For Us", 1),
-	(null, "Reflections Of Magic", 1),
-	(null, "Dance With Her Own", 1),
-	(null, "Troubles Of My Inner Fire", 2),
-	(null, "Time Fireworks", 2),
-	(null, "Magic Circus", 3),
-	(null, "Honey, So Do I", 3),
-	(null, "Sweetie, Let's Go Wild", 3),
-	(null, "She Knows", 3),
-	(null, "Fantasy For Me", 4),
-	(null, "Celebration Of More", 4),
-	(null, "Rock His Everything", 4),
-	(null, "Home Forever", 4),
-	(null, "Diamond Power", 4),
-	(null, "Honey, Let's Be Silly", 4),
-	(null, "Thang Of Thunder", 5),
-	(null, "Words Of Her Life", 5),
-	(null, "Without My Streets", 5);
+values
+(1, "Soul For Us", 1),
+(null, "Reflections Of Magic", 1),
+(null, "Dance With Her Own", 1),
+(null, "Troubles Of My Inner Fire", 2),
+(null, "Time Fireworks", 2),
+(null, "Magic Circus", 3),
+(null, "Honey, So Do I", 3),
+(null, "Sweetie, Let's Go Wild", 3),
+(null, "She Knows", 3),
+(null, "Fantasy For Me", 4),
+(null, "Celebration Of More", 4),
+(null, "Rock His Everything", 4),
+(null, "Home Forever", 4),
+(null, "Diamond Power", 4),
+(null, "Honey, Let's Be Silly", 4),
+(null, "Thang Of Thunder", 5),
+(null, "Words Of Her Life", 5),
+(null, "Without My Streets", 5);
 
 insert into historicos (usuario_id, musica_id)
-	values
-	(1,1),
-	(1,6),
-	(1,14),
-	(1,16),
-	(2,13),
-	(2,17),
-	(2,2),
-	(2,15),
-	(3,4),
-	(3,16),
-	(3,6),
-	(4,3),
-	(4,18),
-	(4,11);
+values
+(1,1),
+(1,6),
+(1,14),
+(1,16),
+(2,13),
+(2,17),
+(2,2),
+(2,15),
+(3,4),
+(3,16),
+(3,6),
+(4,3),
+(4,18),
+(4,11);
 
 insert into seguindo_artistas (usuario_id, artista_id)
-	values
-	(1,1),
-	(1,2),
-	(1,3),
-	(2,1),
-	(2,3),
-	(3,4),
-	(3,1),
-	(4,2);
+values
+(1,1),
+(1,2),
+(1,3),
+(2,1),
+(2,3),
+(3,4),
+(3,1),
+(4,2);
 
 insert into planos (usuario_id, plano, valor_plano)
-	values
-	(1, "gratuito", 0),
-	(2, "familiar", 0),
-	(3, "universitário", 0),
-	(4, "gratuito", 0);
-    
+values
+(1, "gratuito", 0),
+(2, "familiar", 0),
+(3, "universitário", 0),
+(4, "gratuito", 0);
