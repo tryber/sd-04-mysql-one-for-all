39a41314028e4e9dd6efaeebf3ee1b3469c6479a
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-09-03 00:35:09 -0300

    testando com declare

--- desafio10.sql
+++ desafio10.sql
@@ -1,24 +1,21 @@
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico(nome_usuario varchar(50))
-RETURNS int reads SQL data
+CREATE FUNCTION quantidade_musicas_no_historico(nome_usuario VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN
-RETURN (
-    SELECT
-        COUNT(cancao_id)
-    FROM
-        historico_reproducao
-    WHERE
-        (
-            SELECT
-                nome
-            FROM
-                usuario
-            WHERE
-                id = usuario_id
-        ) = nome_usuario
-    GROUP BY
-        usuario_id
-);
+DECLARE reproducoes INT;
+SELECT 
+    COUNT(cancao_id)
+INTO reproducoes FROM
+    historico_reproducao
+WHERE
+    (SELECT 
+            nome
+        FROM
+            usuario
+        WHERE
+            id = usuario_id) = nome_usuario
+GROUP BY usuario_id;
+RETURN reproducoes;
 
-END $$
+END;$$
 DELIMITER ;
