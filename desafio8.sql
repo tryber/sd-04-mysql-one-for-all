31b954c302154e703c6569a2d4bcc1d0c414706a
henrique <hbaetaleite@gmail.com>
2020-09-03 19:25:56 -0300

    mais uma...

--- desafio8.sql
+++ desafio8.sql
@@ -6,8 +6,8 @@ BEFORE DELETE ON SpotifyClone.user
 FOR EACH ROW
 BEGIN
   DELETE FROM SpotifyClone.song_history
-    WHERE SpotifyClone.song_history.user_id = OLD.id;
+    WHERE SpotifyClone.song_history.user_id = OLD.user_id;
   DELETE FROM SpotifyClone.followed_artist
-    WHERE SpotifyClone.followed_artist.user_id = OLD.id;
+    WHERE SpotifyClone.followed_artist.user_id = OLD.user_id;
 END
 $$ DELIMITER ;
