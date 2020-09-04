2da06066838e6c767c5befd528ba5a28d60f9848
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-03 23:42:02 -0300

    cc

--- desafio9.sql
+++ desafio9.sql
@@ -2,9 +2,9 @@ DELIMITER $$
 
 CREATE PROCEDURE albuns_do_artista(IN name VARCHAR(50))
 BEGIN
-    SELECT ar.artist_name `artista`, al.album_name `album` 
-	FROM artist ar 
-	INNER JOIN album al WHERE ar.artist_name = name AND ar.id = al.artist_id;
+SELECT ar.artist_name `artista`, al.album_name `album` 
+FROM artist ar 
+INNER JOIN album al WHERE ar.artist_name = name AND ar.id = al.artist_id;
 END; $$
 
 CALL albuns_do_artista('Walter Phoenix');
