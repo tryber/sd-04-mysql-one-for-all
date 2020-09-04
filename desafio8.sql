23ae2eac7a842a5c2753ae19c5e96f2a9f1e2ed7
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-09-04 00:07:28 -0300

    desafio 10

--- desafio8.sql
+++ desafio8.sql
@@ -3,6 +3,6 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON users
 FOR EACH ROW
 BEGIN
-    DELETE FROM history AS h WHERE h.user_id = OLD.id;
+    DELETE FROM history AS hi WHERE hi.user_id = OLD.id;
     DELETE FROM following AS f WHERE f.user_id = OLD.id;
 END; $$
