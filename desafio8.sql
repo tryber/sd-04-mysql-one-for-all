583595ae540c50be6e473519cdd6047975565aaa
Lucas Ribas <lucas@mundoribas.com.br>
2020-09-05 16:14:51 -0300

    Refazendo os desafios 8 e 9

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,8 @@
-DELIMITER $$
-CREATE TRIGGER trigger_usuario_delete 
-BEFORE DELETE ON usuarios
-FOR EACH ROW 
-BEGIN
-DELETE FROM historico_reproducoes
-  WHERE usuario_id = OLD.id;
-DELETE FROM seguindo_artistas
-  WHERE usuario_id = OLD.id;
+USE SpotifyClone;
+DELIMITER $$ CREATE TRIGGER trigger_usuario_delete BEFORE DELETE ON usuarios FOR EACH ROW BEGIN
+DELETE FROM SpotifyClone.historico_reproducoes
+WHERE usuario_id = OLD.id;
+DELETE FROM SpotifyClone.seguindo_artistas
+WHERE usuario_id = OLD.id;
 END;
 $$ DELIMITER;
--- desafio9.sql
+++ desafio9.sql
@@ -1,11 +1,9 @@
-DELIMITER $$
-CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(30))
-BEGIN
+USE SpotifyClone;
+DELIMITER $$ CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(100)) BEGIN
 SELECT artistas.nome AS artista,
-  albuns.nome AS album
-FROM albuns
-  JOIN artista AS a 
-  ON a.artista_id = albuns.artista_id
-WHERE artista = a.nome;
-END
-$$ DELIMITTER;
+  albuNs.nome AS album
+FROM SpotifyClone.artistas AS artistas
+  INNER JOIN SpotifyClone.albuns AS albuns ON artistas.id = albuns.id_artista
+WHERE artistas.nome = nome
+ORDER BY album;
+END $$ DELIMITER;
