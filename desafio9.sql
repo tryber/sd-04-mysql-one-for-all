7e6358d808537948a93157a2d35b6966dbc0b181
Thabata <dornelasthabata@gmail.com>
2020-09-05 14:18:40 -0300

    Desafios finais feitos

---
+++ desafio9.sql
@@ -0,0 +1,17 @@
+USE SpotifyClone;
+DELIMITER //
+
+CREATE PROCEDURE albuns_do_artista (IN nomeArtista VARCHAR(50))
+BEGIN
+SELECT 
+    A.nome AS 'artista', AL.titulo AS 'album'
+FROM
+    SpotifyClone.artistas AS A
+        INNER JOIN
+    SpotifyClone.albuns AS AL ON A.artista_id = AL.artista_id AND A.nome = nomeArtista
+ORDER BY `album`;
+END //
+
+DELIMITER ;
+
+CALL albuns_do_artista('Walter Phoenix');
