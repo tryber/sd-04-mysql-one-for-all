8aec0710b83e1e4f1dbb999899245ba649ca17bb
igorgbr <igorgphotoarte@gmail.com>
2020-09-04 21:40:28 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+view top_3_artistas as
+select a.artista artistas, count(sa.idartista) seguidores
+from artistas a
+inner join seguindo_artista sa on sa.idartista = a.idartista
+group by artistas
+order by seguidores desc, artistas
+limit 3;
