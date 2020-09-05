7edf54003dfa148b31da66181526a4a93cc3b618
Lucas Ribas <lucas@mundoribas.com.br>
2020-09-05 15:57:37 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+USE SpotifyClone;
+
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete 
+BEFORE DELETE ON usuarios
+FOR EACH ROW 
+BEGIN
+DELETE FROM historico_reproducoes
+  WHERE usuario_id = OLD.id;
+DELETE FROM seguindo_artistas
+  WHERE usuario_id = OLD.id;
+END;
+$$ DELIMITER;
