3ea6e0ee9840284157e098c0eb0fbde85820ae3c
Kaecio <kaecio@yahoo.com.br>
2020-09-05 13:38:33 -0300

    Exibe as estatísticas musicais, CC

--- desafio2.sql
+++ desafio2.sql
@@ -8,8 +8,6 @@ on album.album_id = cancao.album_id
 inner join artista
 on artista.artista_id = album.artista_id;
 
-
-
 /* Crie uma VIEW chamada estatisticas_musicais que exiba três colunas:
 A primeira coluna deve exibir a quantidade total de canções. Dê a essa coluna o alias "cancoes".
 A segunda coluna deve exibir a quantidade total de artistas e deverá ter o alias "artistas".
