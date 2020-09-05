d1835834588d62c3ad618dc5ab4c1442db73e9e4
igorgbr <igorgphotoarte@gmail.com>
2020-09-04 21:44:00 -0300

    desafio 4 - corrigido

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-view top_3_artistas as
+create view top_3_artistas as
 select a.artista artistas, count(sa.idartista) seguidores
 from artistas a
 inner join seguindo_artista sa on sa.idartista = a.idartista
