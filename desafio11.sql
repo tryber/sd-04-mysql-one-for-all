afc6f19e82068a4d08fc5a05ce131150165906cb
igorgbr <igorgphotoarte@gmail.com>
2020-09-05 00:56:25 -0300

    desafio 11 corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,12 @@
-select * from usuarios; select * from historico;
-
-SELECT 
-    c.cancao nome, COUNT(h.idcancao) reproducoes
-FROM
-    historico h
-        INNER JOIN
-    cancoes c ON c.idcancao = h.idcancao
-        INNER JOIN
-    usuarios u ON (u.idusuario = h.idusuario)
-        AND (u.idplano = 2 OR u.idplano = 3)
-GROUP BY c.cancao , h.idusuario
-ORDER BY nome;
+CREATE VIEW cancoes_premium AS
+    SELECT 
+        c.cancao nome, COUNT(h.idcancao) reproducoes
+    FROM
+        historico h
+            INNER JOIN
+        cancoes c ON c.idcancao = h.idcancao
+            INNER JOIN
+        usuarios u ON (u.idusuario = h.idusuario)
+            AND (u.idplano = 2 OR u.idplano = 3)
+    GROUP BY c.cancao , h.idusuario
+    ORDER BY nome;
