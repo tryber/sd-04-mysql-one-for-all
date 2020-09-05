64900dc58ec8434226ddc9a6af55be15dd11d287
igorgbr <igorgphotoarte@gmail.com>
2020-09-05 00:22:45 -0300

    refatorando alguns e desafio 10

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,10 @@
-create view top_2_hits_do_momento as
-select c.cancao cancao, count(h.idcancao) reproducoes
-from cancoes c
-inner join historico h on h.idcancao = c.idcancao
-group by cancao
-order by reproducoes desc, cancao
-limit 2;
+CREATE VIEW top_2_hits_do_momento AS
+    SELECT 
+        c.cancao cancao, COUNT(h.idcancao) reproducoes
+    FROM
+        cancoes c
+            INNER JOIN
+        historico h ON h.idcancao = c.idcancao
+    GROUP BY cancao
+    ORDER BY reproducoes DESC , cancao
+    LIMIT 2;
--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,10 @@
-create view faturamento_atual as
-select min(p.valor_plano) faturamento_minimo, max(p.valor_plano) faturamento_maximo, round(avg(p.valor_plano), 2) faturamento_medio, sum(p.valor_plano) faturamento_total
-from usuarios u
-inner join planos p on p.idplano = u.idplano;
+CREATE VIEW faturamento_atual AS
+    SELECT 
+        MIN(p.valor_plano) faturamento_minimo,
+        MAX(p.valor_plano) faturamento_maximo,
+        ROUND(AVG(p.valor_plano), 2) faturamento_medio,
+        SUM(p.valor_plano) faturamento_total
+    FROM
+        usuarios u
+            INNER JOIN
+        planos p ON p.idplano = u.idplano;
