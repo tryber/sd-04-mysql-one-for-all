0adf91b99b464adfcbcf3ad33df6afd5c94404b2
Lucas Ribas <lucas@mundoribas.com.br>
2020-09-05 16:51:45 -0300

    Quase foi, mas já foi!

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 USE SpotifyClone;
 DELIMITER $$ CREATE PROCEDURE albuns_do_artista(IN nome VARCHAR(30)) BEGIN
 SELECT artistas.nome AS artista,
-  albuns.nome AS album
+  albuns.album AS album
 FROM SpotifyClone.artistas AS artistas
-  INNER JOIN SpotifyClone.albuns AS albuns ON artistas.id = albuns.id_artista
+  INNER JOIN SpotifyClone.albuns AS albuns ON artistas.id_artista = albuns.id_artista
 WHERE artistas.nome = nome
 ORDER BY album;
 END $$ DELIMITER;
