a7dd3951b55e2591a7016c9ad04025219c92ca90
wistyk <leonardoxw@gmail.com>
2020-09-06 11:29:40 -0300

    retirado o artista das cancoes, alterado aspas simples para crase (exceto nos inserts) e colocado crase em todas as strings. Corrigido aspas simples em um numero e acho que adicionado algo em algum lugar que eu esqueci agora

--- desafio1.sql
+++ desafio1.sql
@@ -4,78 +4,77 @@ CREATE DATABASE SpotifyClone;
 
 USE SpotifyClone;
 
-CREATE TABLE planos(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(25) NOT NULL,
-    valor DECIMAL(6, 2)
+CREATE TABLE `planos`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `nome` VARCHAR(25) NOT NULL,
+    `valor` DECIMAL(6, 2)
 );
 
-CREATE TABLE artistas(
-id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
+CREATE TABLE `artistas`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `nome` VARCHAR(50) NOT NULL,
 );
 
-CREATE TABLE usuarios(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(15) NOT NULL,
-    idade INT NOT NULL,
-    plano_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (plano_id) REFERENCES planos(id)
+CREATE TABLE `usuarios`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `nome` VARCHAR(15) NOT NULL,
+    `idade` INT NOT NULL,
+    `plano_id` INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (`plano_id`) REFERENCES `planos`(`id`)
 );
 
-CREATE TABLE album(
-id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    artista_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (artista_id) REFERENCES artistas(id)
+CREATE TABLE `album`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `nome` VARCHAR(50) NOT NULL,
+    `artista_id` INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (`artista_id`) REFERENCES `artistas`(`id`)
 );
 
-CREATE TABLE cancoes(
-id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    album_id INT NOT NULL,
-    artista_id INT NOT NULL,
-    CONSTRAINT FOREIGN KEY (album_id) REFERENCES album(id),
-    CONSTRAINT FOREIGN KEY (artista_id) REFERENCES artista(id)
+CREATE TABLE `cancoes`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `nome` VARCHAR(50) NOT NULL,
+    `album_id` INT NOT NULL,
+    `artista_id` INT NOT NULL,
+    CONSTRAINT FOREIGN KEY (`album_id`) REFERENCES `album`(`id`)
 );
     
-    CREATE TABLE seguindo(
-    id INT NOT NULL,
-    artista_id INT NOT NULL,
-    PRIMARY KEY ('id', 'artista_id'),
-    CONSTRAINT FOREIGN KEY (id) REFERENCES usuarios(id)
-    CONSTRAINT FOREIGN KEY (artista_id) REFERENCES artista(id)
+    CREATE TABLE `seguindo`(
+    `usuarios_id` INT NOT NULL,
+    `artista_id` INT NOT NULL,
+    PRIMARY KEY (`usuarios_id`, `artista_id`),
+    CONSTRAINT FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios`(`id`),
+    CONSTRAINT FOREIGN KEY (`artista_id`) REFERENCES `artista`(`id`)
 );
 
-CREATE TABLE historico(
-    id INT NOT NULL,
-    nome_id INT NOT NULL,
-    PRIMARY KEY('id', 'nome_id'),
-    CONSTRAINT FOREIGN KEY (id) REFERENCES usuarios(id),
-    CONSTRAINT FOREIGN KEY (nome_id) REFERENCES cancoes(id)
+CREATE TABLE `historico`(
+    `usuario_id` INT NOT NULL,
+    `cancao_id` INT NOT NULL,
+    PRIMARY KEY(`id`, `nome_id`),
+    CONSTRAINT FOREIGN KEY (`usuario_id`) REFERENCES `usuarios`(`id`),
+    CONSTRAINT FOREIGN KEY (`cancao_id`) REFERENCES `cancoes`(`id`)
 );
 
-INSERT INTO planos (nome, valor)
+INSERT INTO `planos` (`nome`, `valor`)
 VALUES
   ('gratuito', 0),
   ('familiar', 7.99),
-  ('universitario', '5.99');
+  ('universitario', 5.99);
 
-INSERT INTO artistas (nome)
+INSERT INTO `artistas` (`nome`)
 VALUES
   ('Walter Phoenix'),
   ('Peter Strong'),
   ('Lance Day'),
   ('Freedie Shannon');
 
-INSERT INTO usuarios (nome, idade, plano)
+INSERT INTO `usuarios` (`nome`, `idade`, `plano_id`)
 VALUES
   ('Thati', 23, 1),
   ('Cintia', 35, 2),
   ('Bill', 20, 3),
   ('Roger', 45, 1);
 
-INSERT INTO album (nome, artista_id)
+INSERT INTO `album` (`nome`, `artista_id`)
 VALUES
   ('Envious', 1),
   ('Exuberant', 1),
@@ -83,40 +82,40 @@ VALUES
   ('Incandescent', 3),
   ('Temporary Culture', 4);
   
-INSERT INTO cancoes (nome, album_id, artista_id)
+INSERT INTO `cancoes` (`nome`, `album_id`)
 VALUES
-  ('Soul For Us', 1, 1),
-  ('Reflections Of Magic', 1, 1),
-  ('Dance With Her Own', 1, 1),
-  ('Troubles Of My Inner Fire', 2, 1),
-  ('Time Fireworks', 2, 1),
-  ('Magic Circus', 3, 2),
-  ('Honey, So Do I', 3, 2),
-  ("Sweetie, Let's Go Wild", 3, 2),
-  ('She Knows', 3, 2),
-  ('Fantasy For Me', 4, 3),
-  ('Celebration Of More', 4, 3),
-  ('Rock His Everything', 4, 3),
-  ('Home Forever', 4, 3),
-  ('Diamond Power', 4, 3),
-  ("Honey, Let's Be Silly", 4, 3),
-  ('Thang Of Thunder', 5, 4),
-  ('Words Of Her Life', 5, 4),
-  ('Without My Streets', 5, 4);
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
   
-  INSERT INTO seguindo (artista_id)
+  INSERT INTO `seguindo` (`usuarios_id`, `artista_id`)
   VALUES
-  (1),
-  (4),
-  (3),
-  (1),
-  (3),
-  (2),
-  (1),
-  (4);
+	(1, 1),
+	(1, 4),
+	(1, 3),
+	(2, 1),
+	(2, 3),
+	(3, 2),
+	(3, 1),
+	(4, 4);
   
   INSERT INTO
-  historico (id, nome_id)
+  `historico` (`usuario_id`, `nome_id`)
 VALUES
   (1, 1),
   (1, 6),
