7a05e4bd8d79f89c5e8b8a6a5a9c7d5c54872e21
wistyk <leonardoxw@gmail.com>
2020-09-06 11:35:49 -0300

    CC

--- desafio1.sql
+++ desafio1.sql
@@ -105,14 +105,14 @@ VALUES
   
   INSERT INTO `seguindo` (`usuarios_id`, `artista_id`)
   VALUES
-	(1, 1),
-	(1, 4),
-	(1, 3),
-	(2, 1),
-	(2, 3),
-	(3, 2),
-	(3, 1),
-	(4, 4);
+	(1,1),
+	(1,4),
+	(1,3),
+	(2,1),
+	(2,3),
+	(3,2),
+	(3,1),
+	(4,4);
   
   INSERT INTO
   `historico` (`usuario_id`, `nome_id`)
