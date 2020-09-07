e7eb66a8ae7a178a81fa6a1608cd3d84363fa49c
Kaecio <kaecio@yahoo.com.br>
2020-09-07 00:28:20 -0300

    Exibe o histórico de reprodução para cada pessoa usuária,CC

--- desafio3.sql
+++ desafio3.sql
@@ -13,5 +13,4 @@ A primeira coluna deve possuir o alias "usuario" e exibir o nome da pessoa usuá
 A segunda coluna deve possuir o alias "nome" e exibir o nome da canção ouvida pela pessoa 
 com base no seu histórico de reprodução.
 Os resultados devem estar ordenados por nome da pessoa usuária em ordem alfabética
- e em caso de empate no nome os resultados devem ser ordenados pelo nome da canção em ordem alfabética.*/
- 
+e em caso de empate no nome os resultados devem ser ordenados pelo nome da canção em ordem alfabética.*/
