f927d5a7d45652f32ddcecdc31ba536ba0b6c842
wistyk <leonardoxw@gmail.com>
2020-09-06 21:56:48 -0300

    daqueles que com certeza tem uma maneira mais inteligente, mas...

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,7 @@
 CREATE VIEW faturamento_atual AS
 SELECT MIN(plan_price) AS 'faturamento_minimo',
-MAX(plan_price) AS 'faturamento_maximo',
-ROUND(AVG(plan_price), 2) AS 'faturamento_medio',
+MAX(plan_price) AS 'faturamento_maximo', (
+SELECT ROUND(SUM(plan_price)/COUNT(SpotifyClone.user.user_id), 2)
+FROM SpotifyClone.user ) AS 'faturamento_medio',
 SUM(plan_price) AS 'faturamento_total'
 FROM SpotifyClone.plan;
