b9abeecae0998bdcaea34fbccd33ca9dc0b70e96
Caio Magalhães <caio.lmf@gmail.com>
2020-09-07 16:27:43 -0300

    Do challenge 6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+-- Drop view if neccesary
+DROP VIEW IF EXISTS faturamento_atual;
+
+-- Create historico_reproducao_usuarios view
+CREATE VIEW faturamento_atual AS
+SELECT MIN(s.price) as faturamento_minimo,
+    MAX(s.price) AS faturamento_maximo,
+    ROUND(AVG(s.price), 2) AS faturamento_medio,
+    SUM(s.price) AS faturamento_total
+FROM users AS u
+    INNER JOIN subscriptions AS s on s.subscription_id = u.subscription;
