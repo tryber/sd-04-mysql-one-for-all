4b479416a66b96d8f23dd12f440b677993acc884
Julio Cesar <stininmatic@hotmail.com>
2020-09-07 10:36:41 -0300

    desafio2

--- desafio2.sql
+++ desafio2.sql
@@ -1,4 +1,4 @@
-create view teste2 as
+create view desafio2 as
 select count(m.musica_id) as cancoes,
 (select count(ar.artista_id) from SpotifyClone.artistas ar) artistas,
 (select count(a.album_id) albuns from SpotifyClone.albuns a) albuns
