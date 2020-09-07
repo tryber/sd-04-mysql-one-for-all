dda68028192388560ad9306dad4df2f59798d85f
wistyk <leonardoxw@gmail.com>
2020-09-07 12:25:32 -0300

    end sem begin, rs. adicionado in

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,6 @@
 DELIMITER $$
-CREATE PROCEDURE albuns_do_artista (nome VARCHAR(50))
+CREATE PROCEDURE albuns_do_artista (IN nome VARCHAR(50))
+BEGIN
 SELECT (
 SELECT artist_name FROM SpotifyClone.artist
 WHERE artist_id = SpotifyClone.album.artist_id) AS 'artista',
