2fd83b9a02a808abd0a6e18a9038d5fcf82df799
Caio Magalhães <caio.lmf@gmail.com>
2020-09-07 22:05:43 -0300

    correct syntax on challenge 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 -- Create historico_reproducao_usuarios view
 CREATE VIEW historico_reproducao_usuarios AS (
-    SELECT s.name AS nome
+    SELECT s.name AS nome,
         u.username AS usuario,
     FROM users AS u
         INNER JOIN songs_history AS sh ON sh.user = u.user_id
