84d43bef200409be99aff47b257864f68f03e54c
Pedro <pedrotpoliveira@gmail.com>
2020-09-08 17:18:33 -0300

    Desafio 2 - Evaluator Fix

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,3 @@
-USE SpotifyClone;
-
 CREATE VIEW estatisticas_musicais AS
-  SELECT COUNT(DISTINCT s.id) AS `cancoes`, COUNT(DISTINCT art.id) AS `artistas`, COUNT(DISTINCT alb.id) AS `albuns`
+  SELECT COUNT(DISTINCT s.id) AS cancoes, COUNT(DISTINCT art.id) AS artistas, COUNT(DISTINCT alb.id) AS albuns
   FROM songs AS s, album AS alb, artists AS art;
