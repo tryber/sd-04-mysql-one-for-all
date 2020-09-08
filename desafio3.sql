97a8ead9ef0a015b781f851f1e834f3e59aebd94
Caio Magalhães <caio.lmf@gmail.com>
2020-09-07 22:33:10 -0300

    cast decimal as char on chall 6

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,7 @@
 -- Create historico_reproducao_usuarios view
 CREATE VIEW historico_reproducao_usuarios AS (
     SELECT s.name AS nome,
-        u.username AS usuario,
+        u.username AS usuario
     FROM users AS u
         INNER JOIN songs_history AS sh ON sh.user = u.user_id
         INNER JOIN songs AS s ON s.song_id = sh.song
--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 -- Create faturamento_atual view
 CREATE VIEW faturamento_atual AS (
-    SELECT MAX(s.price) AS faturamento_maximo,
-        ROUND(AVG(s.price), 2) AS faturamento_medio,
-        MIN(s.price) as faturamento_minimo,
-        SUM(s.price) AS faturamento_total
+    SELECT CAST(MAX(s.price) AS CHAR) AS faturamento_maximo,
+        CAST(ROUND(AVG(s.price), 2)  AS CHAR) AS faturamento_medio,
+        CAST(MIN(s.price)  AS CHAR) as faturamento_minimo,
+        CAST(SUM(s.price)  AS CHAR) AS faturamento_total
     FROM users AS u
         INNER JOIN subscriptions AS s ON s.subscription_id = u.subscription
 );
