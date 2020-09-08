340b862aea84515f78a83fc3e46a6a3ff09df8b1
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-08 12:49:05 -0300

    ae

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
 CREATE VIEW faturamento_atual AS 
-SELECT MIN(valor_plano) AS faturamento_minimo,
+SELECT 
+MIN(valor_plano) AS faturamento_minimo,
 MAX(valor_plano) AS faturamento_maximo,
-ROUND(AVG(valor_plano),2) AS faturamento_medio,
-ROUND(SUM(valor_plano),2) AS faturamento_total
+CAST(ROUND(AVG(valor_plano),2)) AS faturamento_medio,
+CAST(ROUND(SUM(valor_plano),2)) AS faturamento_total
 FROM SpotifyClone.planos
 INNER JOIN SpotifyClone.usuarios AS U
 ON planos.planos_id = U.plano_id;
