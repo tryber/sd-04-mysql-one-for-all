d93e5d81280c052ec7f187747ead1c9f3752bfbb
Kaecio <kaecio@yahoo.com.br>
2020-09-08 19:21:31 -0300

    Crie uma trigger chamada trigger_usuario_delete que deve ser disparada sempre que uma pessoa usuária for excluída do banco de dados,CC

--- desafio8.sql
+++ desafio8.sql
@@ -3,11 +3,12 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuario 
 FOR EACH ROW
 BEGIN
-DELETE FROM seguir_artista 
+DELETE FROM SpotifyClone.seguir_artista 
 WHERE usuario_id = OLD.usuario_id;
-DELETE FROM historico_reproducao
+DELETE FROM SpotifyClone.historico_reproducao
 WHERE usuario_id = OLD.usuario_id;
 END $$
+DELIMITER ;
 
 /*Crie uma trigger chamada trigger_usuario_delete que deve ser disparada sempre que 
 uma pessoa usuária for excluída do banco de dados, refletindo essa exclusão em todas as 
