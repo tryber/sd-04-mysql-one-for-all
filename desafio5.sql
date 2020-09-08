2993518a00d98ce9275e4ee31ae43ca203d62cdd
lourenco <loulage@gmail.com>
2020-09-08 16:21:13 -0300

    4 e 5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+USE SpotifyClone;
+CREATE VIEW top_2_hits_do_momento AS (
+  SELECT c.nome AS cancao, COUNT(h.cancao_id) AS reproducoes FROM SpotifyClone.cancoes AS c
+    INNER JOIN SpotifyClone.historico_reproducoes AS h
+    ON c.id = h.cancao_id
+    GROUP BY h.cancao_id
+    ORDER BY reproducoes DESC, cancao
+    LIMIT 2
+);
