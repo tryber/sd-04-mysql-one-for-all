8140fe96e74a1062802278f7713dc0c602d75bd6
lourenco <loulage@gmail.com>
2020-09-08 16:33:47 -0300

    desafio6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+USE SpotifyCLone;
+CREATE VIEW faturamento_atual AS(
+  SELECT MIN(p.valor) AS faturamento_minimo,
+    MAX(p.valor) AS faturamento_maximo,
+    ROUND(AVG(p.valor), 2) AS faturamento_medio,
+    SUM(p.valor) AS faturamento_total
+    FROM SpotifyClone.planos AS p
+    INNER JOIN SpotifyClone.usuarios AS u
+    ON u.id_plano = p.id
+);
