93bae43f687b8b011030e8bb4f020523f5af74d1
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-09-08 16:38:51 -0300

    Todos ok!

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
 CREATE VIEW historico_reproducao_usuarios AS
-SELECT user_name AS `usuario`, song_name AS `nome`
-FROM spotifyclone.user AS us
-INNER JOIN spotifyclone.song_history AS soh
-ON us.user_id = soh.user_id
-INNER JOIN spotifyclone.song AS sg
-ON soh.song_id = sg.song_id
+SELECT u.user_name AS `usuario`, s.song_name AS `nome`
+FROM SpotifyClone.user AS u
+INNER JOIN SpotifyClone.song_history AS sh
+ON u.user_id = sh.user_id
+INNER JOIN SpotifyClone.song AS s
+ON s.song_id = sh.song_id
 ORDER BY `usuario`, `nome`;
--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
 CREATE VIEW faturamento_atual AS
 SELECT 
-MIN(pl.plan_price) AS faturamento_minimo,
-MAX(pl.plan_price) AS faturamento_maximo,
+ROUND(MIN(pl.plan_price), 2) AS faturamento_minimo,
+ROUND(MAX(pl.plan_price), 2) AS faturamento_maximo,
 ROUND(AVG(pl.plan_price), 2) AS faturamento_medio,
-SUM(pl.plan_price) AS faturamento_total
+ROUND(SUM(pl.plan_price), 2) AS faturamento_total
 FROM plan AS pl
 INNER JOIN spotifyclone.user AS us
 ON us.plan_id = pl.plan_id;
