793ded515a513fd502f38fcc953ab86af7ffe9de
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-08 12:32:53 -0300

    quase

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
--- CREATE VIEW faturamento_atual AS 
+CREATE VIEW faturamento_atual AS 
 SELECT MIN(valor_plano) AS faturamento_minimo,
 MAX(valor_plano) AS faturamento_maximo,
 ROUND(AVG(valor_plano),2) AS faturamento_medio,
