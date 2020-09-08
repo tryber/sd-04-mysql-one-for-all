009243f330bb2fe4614248d78fca297c0cd3b4f1
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-09-08 18:39:07 -0300

    desafio 2 - camelcase

--- desafio2.sql
+++ desafio2.sql
@@ -4,7 +4,7 @@ COUNT(DISTINCT son.song_id) AS cancoes,
 COUNT(DISTINCT art.artist_id) AS artistas,
 COUNT(DISTINCT alb.album_id) AS albuns
 FROM spotifyclone.artist AS art
-INNER JOIN spotifyclone.album AS alb
+INNER JOIN spotifyClone.album AS alb
 ON art.artist_id = alb.artist_id
-INNER JOIN spotifyclone.song AS son
+INNER JOIN spotifyClone.song AS son
 ON son.album_id = alb.album_id;
