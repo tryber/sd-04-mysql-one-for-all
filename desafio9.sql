d009a8b5a49de651668632c18b66b1c7b8a272f5
Kaecio <kaecio@yahoo.com.br>
2020-09-08 18:20:14 -0300

    Crie uma trigger chamada trigger_usuario_delete que deve ser disparada sempre que uma pessoa usuária for excluída do banco de dados,CC

--- desafio9.sql
+++
@@ -1,8 +0,0 @@
-/*Crie uma procedure chamada albuns_do_artista que recebe 
-como parâmetro o nome de uma pessoa artista e em retorno deve exibir as seguintes colunas:
-O nome da pessoa artista, com o alias "artista".
-O nome do álbum, com o alias "album".
-Os resultados devem ser ordenados pelo nome do álbum em ordem alfabética.
-Confirme a execução correta da procedure, chamando-a e passando o nome i
-gual a "Walter Phoenix". Sua procedure deve retornar o mesmo que o resultado abaixo:
-CALL albuns_do_artista('Walter Phoenix');*/
