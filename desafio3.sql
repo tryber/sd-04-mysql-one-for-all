800ddc8edad598d413374e70eb13a4ad45725a8c
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-09-08 16:54:24 -0300

    Formatei sql pra ver se passa

--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,9 @@
-CREATE VIEW historico_reproducao_usuarios AS
-SELECT u.user_name AS `usuario`, s.song_name AS `nome`
-FROM SpotifyClone.user AS u
-INNER JOIN SpotifyClone.song_history AS sh
-ON u.user_id = sh.user_id
-INNER JOIN SpotifyClone.song AS s
-ON s.song_id = sh.song_id
-ORDER BY `usuario`, `nome`;
+CREATE VIEW historico_reproducao_usuarios
+AS
+  SELECT u.user_name AS `usuario`, s.song_name AS `nome`
+  FROM SpotifyClone.user AS u
+    INNER JOIN SpotifyClone.song_history AS sh
+    ON u.user_id = sh.user_id
+    INNER JOIN SpotifyClone.song AS s
+    ON s.song_id = sh.song_id
+  ORDER BY `usuario`, `nome`;
--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,11 @@
-CREATE VIEW faturamento_atual AS
-SELECT 
-ROUND(MIN(pl.plan_price), 2) AS faturamento_minimo,
-ROUND(MAX(pl.plan_price), 2) AS faturamento_maximo,
-ROUND(AVG(pl.plan_price), 2) AS faturamento_medio,
-ROUND(SUM(pl.plan_price), 2) AS faturamento_total
-FROM plan AS pl
-INNER JOIN spotifyclone.user AS us
+CREATE VIEW faturamento_atual
+AS
+  SELECT
+    ROUND(MIN(pl.plan_price), 2) AS faturamento_minimo,
+    ROUND(MAX(pl.plan_price), 2) AS faturamento_maximo,
+    ROUND(AVG(pl.plan_price), 2) AS faturamento_medio,
+    ROUND(SUM(pl.plan_price), 2) AS faturamento_total
+  FROM plan AS pl
+    INNER JOIN spotifyclone. user
+AS us
 ON us.plan_id = pl.plan_id;
