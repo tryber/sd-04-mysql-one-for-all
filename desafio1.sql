efb39f1e68cc151295f46162d65f591d8c45db9d
lourenco <loulage@gmail.com>
2020-09-08 15:00:33 -0300

    populate data into spotfyclone schema

--- desafio1.sql
+++ desafio1.sql
@@ -24,8 +24,6 @@ CREATE TABLE IF NOT EXISTS `SpotfyClone`.`usuarios` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-DROP TABLE IF EXISTS `SpotfyClone`.`artistas` ;
-
 CREATE TABLE IF NOT EXISTS `SpotfyClone`.`artistas` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome_artista` VARCHAR(55) NOT NULL,
@@ -93,3 +91,80 @@ CREATE TABLE IF NOT EXISTS `SpotfyClone`.`historico_reproducao` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+INSERT INTO artistas (nome_artista)
+  VALUES
+      ('Walter Phoenix'),
+      ('Peter Strong'),
+      ('Lance Day'),
+      ('Freedie Shannon');
+
+INSERT INTO planos(nome_plano, valor_plano)
+  VALUES
+      ('gratuito', 0),
+      ('familiar', 7.99),
+      ('universitário', 5.99);
+
+INSERT INTO usuarios(nome, idade, id_plano)
+  VALUES
+      ('Thati', 23, 1),
+      ('Cintia', 35, 2),
+      ('Bill', 20, 3),
+      ('Roger', 45, 1);
+
+INSERT INTO albuns(nome_album, id_artista)
+  VALUES
+      ('Envious', 1),
+      ('Exuberant', 1),
+      ('Hallowed Steam', 2),
+      ('Incandescent', 3),
+      ('Temporary Culture', 4);
+
+INSERT INTO cancoes(nome_musica, id_album)
+  VALUES
+      ('Soul For Us', 1),
+      ('Reflections Of Magic', 1),
+      ('Dance With Her Own', 1),
+      ('Troubles Of My Inner Fire', 2),
+      ('Time Fireworks', 2),
+      ('Magic Circus', 3),
+      ('Honey, So Do I', 3),
+      ("Sweetie, Let's Go Wild", 3),
+      ('She Knows', 3),
+      ('Fantasy For Me', 4),
+      ('Celebration Of More', 4),
+      ('Rock His Everything', 4),
+      ('Home Forever', 4),
+      ('Diamond Power', 4),
+      ("Honey, Let's Be Silly", 4),
+      ('Thang Of Thunder', 5),
+      ('Words Of Her Life', 5),
+      ('Without My Streets', 5);
+
+INSERT INTO seguindo_artistas(id_usuario, id_artista)
+  VALUES
+      (1, 1),
+      (1, 3),
+      (1, 4),
+      (2, 1),
+      (2, 3),
+      (3, 1),
+      (3, 2),
+      (4, 4);
+
+INSERT INTO historico_reproducao(id_usuario, id_cancao)
+  VALUES
+      (1, 1),
+      (1, 6),
+      (1, 14),
+      (1, 16),
+      (2, 13),
+      (2, 17),
+      (2, 2),
+      (2, 15),
+      (3, 4),
+      (3, 16),
+      (3, 6),
+      (4, 3),
+      (4, 18),
+      (4, 11);
