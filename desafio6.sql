b4f1699e0eb2a71244b96a3b549714db17a11357
Thiago Tavares <thiagooo15@gmail.com>
2020-09-08 06:47:35 -0300

    ccfix

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 CREATE VIEW faturamento_atual AS (
   SELECT CAST(MAX(s.price) AS CHAR) AS faturamento_maximo,
-		CAST(ROUND(AVG(s.price), 2) AS CHAR) AS faturamento_medio,
+		CAST(ROUND(AVG(s.price), 2)  AS CHAR) AS faturamento_medio,
     CAST(MIN(s.price) AS CHAR) as faturamento_minimo,
     CAST(SUM(s.price) AS CHAR) AS faturamento_total
   FROM SpotifyClone.users AS u
