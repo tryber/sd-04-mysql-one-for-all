409986be351925cbb72da8ec0f2a0779631e01cf
Kaecio <kaecio@yahoo.com.br>
2020-09-08 18:01:14 -0300

    Crie uma trigger chamada trigger_usuario_delete que deve ser disparada sempre que uma pessoa usuária for excluída do banco de dados, faltou espaço no ponto-virgula depois do DELIMITER

--- desafio8.sql
+++ desafio8.sql
@@ -1,14 +1,14 @@
-DELIMITER &&
+DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete 
 BEFORE DELETE ON usuario 
 FOR EACH ROW
 BEGIN
-delete from seguir_artista
+delete from seguir_artista 
 where usuario_id = OLD.usuario_id;
-delete from historico_reproducao
-where usuario= OLD.usuario_id;
-END;
-DELIMITER;
+delete from historico_reproducao 
+where usuario_id = OLD.usuario_id;
+END; $$
+DELIMITER ;
 
 /*Crie uma trigger chamada trigger_usuario_delete que deve ser disparada sempre que 
 uma pessoa usuária for excluída do banco de dados, refletindo essa exclusão em todas as 
