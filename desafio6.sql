21e14357d198762793cd86a4b9f05d702db644a7
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-09-08 12:15:15 -0300

    uau

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 CREATE VIEW faturamento_atual AS 
 SELECT MIN(valor_plano) AS faturamento_minimo,
 MAX(valor_plano) AS faturamento_maximo,
-ROUND(AVG(valor_plano), 2) AS faturamento_medio,
-ROUND(SUM(valor_plano), 2) AS faturamento_total
+AVG(valor_plano) AS faturamento_medio,
+SUM(valor_plano) AS faturamento_total
 FROM SpotifyClone.planos
 INNER JOIN SpotifyClone.usuarios AS U
 ON planos.planos_id = U.plano_id;
--- desafio7.sql
+++ desafio7.sql
@@ -4,4 +4,4 @@ SELECT
 album_name AS album,
 (SELECT COUNT(artista_id) FROM SpotifyClone.seguindo WHERE seguindo.artista_id = album.artista_id) AS seguidores
 FROM SpotifyClone.album
-ORDER BY seguidores DESC, album DESC;
+ORDER BY seguidores DESC, artista ASC, album ASC;
