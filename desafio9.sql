921ebdc6ffd676150b701df8b235c6466001026f
Pedro <pedrotpoliveira@gmail.com>
2020-09-09 13:14:23 -0300

    Desafio 9 - Evaluator Fix

--- desafio9.sql
+++ desafio9.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE albuns_do_artista(IN artist_name VARCHAR(100))
 BEGIN
-    SELECT art.name, alb.title
+    SELECT art.name AS artista, alb.title AS album
     FROM SpotifyClone.album AS alb, SpotifyClone.artists AS art
     WHERE art.id = alb.artist AND art.name = artist_name;
 END $$
