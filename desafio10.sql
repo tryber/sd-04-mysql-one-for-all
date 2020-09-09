0ab68c188418f55becae2c803f234167444e75b8
Pedro <pedrotpoliveira@gmail.com>
2020-09-09 13:21:44 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(artist_name VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE plays_total INT;
+    SELECT COUNT(*)
+    FROM playback_history AS ph, users AS u
+    WHERE ph.user = u.id AND u.name = artist_name INTO plays_total;
+    RETURN plays_total;
+END $$
+
+DELIMITER ;
