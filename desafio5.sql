33a171a7e0243694dc6e28588e6db894e4f75c95
djonata-dias <diasdjonata@gmail.com>
2020-09-09 16:19:22 -0300

    Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,13 @@
+CREATE VIEW top_2_hits_do_momento AS
+SELECT
+    c.titulo AS cancao,
+    COUNT(ms.cancao_id) AS reproducoes
+FROM
+    SpotifyClone.musicas_escutadas ms
+    INNER JOIN SpotifyClone.cancoes c ON c.cancao_id = ms.cancao_id
+GROUP BY
+    c.titulo
+ORDER BY
+    reproducoes DESC
+LIMIT
+    2;
