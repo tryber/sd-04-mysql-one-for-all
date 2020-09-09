81ffdf8b2ec480c63d00fb778f5f7ac0ee167b3e
Pedro <pedrotpoliveira@gmail.com>
2020-09-09 13:27:45 -0300

    Desafio 10 - Evaluator Fix

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,12 @@
-USE SpotifyClone;
-
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(artist_name VARCHAR(100))
+CREATE FUNCTION quantidade_musicas_no_historico(user_id INT)
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE plays_total INT;
     SELECT COUNT(*)
-    FROM playback_history AS ph, users AS u
-    WHERE ph.user = u.id AND u.name = artist_name INTO plays_total;
+    FROM playback_history AS ph
+    WHERE ph.user = user_id INTO plays_total;
     RETURN plays_total;
 END $$
 
