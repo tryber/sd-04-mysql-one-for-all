2ad3cdb39a0b824067aba15287034d5e21b07260
djonata-dias <diasdjonata@gmail.com>
2020-09-09 18:03:28 -0300

    Desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -4,13 +4,13 @@ DELIMITER $ $ DROP TRIGGER IF EXISTS trigger_usuario_delete;
 
 CREATE TRIGGER trigger_usuario_delete BEFORE DELETE ON SpotifyClone.usuarios FOR EACH ROW BEGIN
 DELETE FROM
-    SpotifyClone.artistas_seguidos asg
+    SpotifyClone.artistas_seguidos 
 WHERE
-    asg.usuario_id = OLD.usuario_id;
+    usuario_id = OLD.usuario_id;
 
 DELETE FROM
-    SpotifyClone.musicas_escutadas ms
+    SpotifyClone.musicas_escutadas 
 WHERE
-    ms.usuario_id = OLD.usuario_id;
+    usuario_id = OLD.usuario_id;
 
 END $ $ DELIMITER;
