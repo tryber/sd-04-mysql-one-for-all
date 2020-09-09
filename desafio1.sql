e1a12ac56de0adeb60285e2b98387977505d6ca6
djonata-dias <diasdjonata@gmail.com>
2020-09-09 16:56:23 -0300

    ajuste de dado do banco

--- desafio1.sql
+++ desafio1.sql
@@ -7,7 +7,7 @@ USE SpotifyClone;
 CREATE TABLE planos(
     plano_id INT PRIMARY KEY AUTO_INCREMENT,
     plano VARCHAR(100),
-    valor_plano INT NOT NULL
+    valor_plano DECIMAL NOT NULL
 ) ENGINE = InnoDB;
 
 CREATE TABLE artistas(
@@ -58,7 +58,7 @@ CREATE TABLE artistas_seguidos (
 INSERT INTO
     planos (plano, valor_plano)
 VALUES
-    ("gratuito", 0),
+    ("gratuito", 0.00),
     ("familiar", 7.99),
     ("universitário", 5.99);
 
