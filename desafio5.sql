57b1fcf24f94ec9d3b82f9980bd4ec3c240219a7
Julio Cesar <stininmatic@hotmail.com>
2020-09-09 01:08:47 -0300

    req 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,3 +1,4 @@
+create view top_2_hits_do_momento as
 select m.nome_musica as cancao, count(h.usuario_id) as reproducoes
 from SpotifyClone.musicas m
 inner join SpotifyClone.historicos h on h.musica_id = m.musica_id
