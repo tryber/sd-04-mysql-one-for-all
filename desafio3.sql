32c2c2deb028c11ee7793512ceb73f25ebea0774
gabriellukke <gabriellukke@outlook.com>
2020-09-09 09:01:05 -0300

    Desafio 3 Fix v1.0

--- desafio3.sql
+++ desafio3.sql
@@ -1,5 +1,8 @@
-CREATE VIEW historico_reproducao_usuarios AS (
-SELECT
-(SELECT COUNT(usuario_id) FROM SpotifyClone.usuarios) AS usuario,
-(SELECT COUNT(cancao) FROM SpotifyClone.cancoes) AS nome
-);
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT usuarios.nome AS usuario, cancoes.cancao AS nome
+FROM SpotifyClone.usuarios AS usuarios
+JOIN SpotifyClone.historico_de_reproducoes AS historico
+ON usuarios.usuario_id = historico.usuario_id
+JOIN SpotifyClone.cancoes AS cancoes
+ON cancoes.cancao_id = historico.cancao_id
+ORDER BY usuario, nome;
