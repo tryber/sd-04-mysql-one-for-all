6a34415449cdc65640f113d79690c67302575b52
saturnian99 <saturnian.dsg@gmail.com>
2020-09-12 20:14:18 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,5 @@
+CREATE VIEW estatísticas_musicais AS
+SELECT
+(SELECT COUNT(*) FROM SpotifyClone.cancoes) AS cancoes,
+(SELECT COUNT(*) FROM SpotifyClone.artistas) AS artistas,
+(SELECT COUNT(*) FROM SpotifyClone.albuns) AS albuns
