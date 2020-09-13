371088c4079c14e9da48cd2f6da9bf2e9952c586
saturnian99 <saturnian.dsg@gmail.com>
2020-09-13 19:57:20 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,6 +3,9 @@ SELECT
 c.cancao AS nome,
 COUNT(uc.usuario_id) AS reproducoes
 FROM SpotifyClone.usuarios_cancoes AS uc
-JOIN SpotifyClone.cancoes AS c
-ON c.cancao_id = uc.cancao_id
-GROUP BY nome;
+JOIN SpotifyClone.cancoes AS c ON c.cancao_id = uc.cancao_id
+JOIN SpotifyClone.usuarios AS u ON u.usuario_id = uc.usuario_id
+WHERE u.plano_id IN (2, 3)
+GROUP BY nome
+ORDER BY nome;
+
