4739f0920653c2536dfe060013dc2373b046737a
Josias Viveiro <josiasviveiro@gmail.com>
2020-09-17 12:32:44 -0300

    Desafio8 sintaxe

--- desafio8.sql
+++ desafio8.sql
@@ -4,6 +4,7 @@ Crie uma trigger chamada trigger_usuario_delete que deve ser disparada sempre qu
   for excluída do banco de dados, refletindo essa exclusão em todas as tabelas que ela estiver.
 Teste a funcionalidade correta de sua trigger, fazendo a exclusão da usuária "Thati".
 */
+USE SpotifyClone;
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
   BEFORE DELETE ON usuario
