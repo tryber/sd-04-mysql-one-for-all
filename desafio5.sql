0da73050741c8db7e2f3a868c4041c803292942d
binhus <eng.robsonwilli@gmail.com>
2020-09-18 16:02:37 -0300

    corrigindo desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,5 @@
 CREATE VIEW top_2_hits_do_momento AS (
-  SELECT SpotifyClone.cacoes.cancao_nome AS cancao,
+  SELECT cancoes.cancao_nome AS cancao,
   (
     SELECT COUNT(*) FROM SpotifyClone.historico AS historico
     WHERE (historico.cancao_id = cancoes.cancao_id)
