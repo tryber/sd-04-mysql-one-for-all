b75c694a5463e099c1edab92d052cf4592ba0924
binhus <eng.robsonwilli@gmail.com>
2020-09-30 00:14:27 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,21 @@
+SELECT
+  cancoes.cancao_nome AS nome,
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      historico
+      INNER JOIN usuarios ON (usuarios.usuario_id = historico.usuario_id)
+    WHERE
+      cancoes.cancao_id = historico.cancao_id
+      AND usuarios.plano_id IN (2, 3)
+  ) AS reproducoes
+FROM
+  cancoes
+GROUP BY
+  cancoes.cancao_id
+ORDER BY
+  reproducoes DESC,
+  nome
+LIMIT
+  7;
