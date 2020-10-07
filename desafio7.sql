588d2ca01b385e3f7ae0dc96d04b59fb3d430eb7
samueap <samueap@gmail.com>
2020-10-07 12:22:07 -0300

    fix req 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 CREATE VIEW perfil_artistas AS
 SELECT 
-  art.nome_usuario AS 'artista',
+  art.nome_artista AS 'artista',
     alb.titulo_album AS 'album',
-    COUNT(S.usuario_id) AS 'seguidores'
+    COUNT(seg.usuario_id) AS 'seguidores'
 FROM
     SpotifyClone.artistas AS art INNER JOIN SpotifyClone.albuns AS alb ON art.artista_id = alb.artista_id
     INNER JOIN SpotifyClone.seguindo_artistas AS seg ON seg.artista_id = art.artista_id GROUP BY album_id
