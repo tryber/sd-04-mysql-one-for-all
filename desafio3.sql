057ae1d3609ff6a0520efc965a9ff90ce6d2cd45
samueap <samueap@gmail.com>
2020-10-07 07:31:46 -0300

    fix to evaluation

--- desafio3.sql
+++ desafio3.sql
@@ -1,10 +1,10 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT 
-  usuarios.nome_usuario, cancoes.titulo_cancoes AS nome
+  usuarios.nome_usuario, cancoes.titulo_cancoes AS 'nome'
 FROM
-  SpotifyClone.historico_reproducoes AS reproducoes
+  SpotifyClone.historico_reproducoes AS 'reproducoes'
     LEFT JOIN
-  SpotifyClone.usuarios AS usuarios ON reproducoes.usuario_id = usuarios.usuario_id
+  SpotifyClone.usuarios AS 'usuarios' ON reproducoes.usuario_id = usuarios.usuario_id
     LEFT JOIN
-  SpotifyClone.cancoes AS cancoes ON cancoes.cancao_id = reproducoes.cancao_id
+  SpotifyClone.cancoes AS 'cancoes' ON cancoes.cancao_id = reproducoes.cancao_id
 ORDER BY 1 , 2;
